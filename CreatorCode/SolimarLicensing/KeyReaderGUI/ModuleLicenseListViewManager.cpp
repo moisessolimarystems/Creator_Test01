4848e464c951e194902db4d67738c528ca727a6c
mksuser <systems@solimarsystems.com>
2007-02-05 07:00:54 -0800

    CR.FIX.7697 added error checks to catch unhandled exceptions

--- CreatorCode/SolimarLicensing/KeyReaderGUI/ModuleLicenseListViewManager.cpp
+++ CreatorCode/SolimarLicensing/KeyReaderGUI/ModuleLicenseListViewManager.cpp
@@ -87,12 +87,12 @@ void ModuleLicenseListViewManager::FillRow(ModuleLicensingStructure TheModuleLic
 	}
 	else
 	{
-		sprintf_s(retval, sizeof(retval), "%d", TheModuleLicStructure.TotalLicenses);
+		sprintf(retval, "%d", TheModuleLicStructure.TotalLicenses);
 		listViewItem1->SubItems->Add(retval);
 	}
 
 	//convert the licenses in use to a string and insert it as a subitem
-	sprintf_s(retval, sizeof(retval), "%d", TheModuleLicStructure.LicensesInUse);
+	sprintf(retval, "%d", TheModuleLicStructure.LicensesInUse);
 	listViewItem1->SubItems->Add(retval);
 
 	ListViewItem* __mcTemp__2[] = new ListViewItem*[1];
