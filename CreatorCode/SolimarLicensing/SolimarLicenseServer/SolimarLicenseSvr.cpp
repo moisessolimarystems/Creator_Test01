180b6d6242b0531ffc545d3b747583c1487de532
jlan <jeff.lan@solimarsystems.com>
2007-04-25 08:08:37 -0700

    CR.FIX.8071.v1 - Removed Resync Key call, causes license server to spike. Will need to expose a new functionality through the interface to allow creator to refresh the key list for parallel keys.

--- CreatorCode/SolimarLicensing/SolimarLicenseServer/SolimarLicenseSvr.cpp
+++ CreatorCode/SolimarLicensing/SolimarLicenseServer/SolimarLicenseSvr.cpp
@@ -77,7 +77,6 @@ STDMETHODIMP CSolimarLicenseSvr::Heartbeat()
 STDMETHODIMP CSolimarLicenseSvr::KeyEnumerate(VARIANT *keylist)
 {
 	CHECK_CLIENT_AUTHENTICATION;
-	keyserver.ResynchronizeKeys();
 	return keyserver.KeyEnumerate(keylist);
 }
 
