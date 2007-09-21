8ea69c3321dc3baf4cdc59e129339c358c843168
Alan Chou <alan.chou@solimarsystems.com>
2007-09-21 04:24:20 -0700

    added display of connected license server version

--- CreatorCode/SolimarLicensing/KeyReaderGUI/AboutBox.h
+++ CreatorCode/SolimarLicensing/KeyReaderGUI/AboutBox.h
@@ -33,6 +33,8 @@ namespace KeyReaderGUI
 			InitializeDynamicData();
 		}
         
+		void SetServerVersion(String* version){versionServer->Text = version;};
+
 	protected: 
 		void Dispose(Boolean disposing)
 		{
@@ -47,7 +49,7 @@ namespace KeyReaderGUI
 			String* year = System::DateTime::Now.Year.ToString();
 			int index = (labelCopyright->Text)->IndexOf("XXXX");
 			labelCopyright->Text = labelCopyright->Text->Remove(index, 4)->Insert(index, year);
-			versionLicensing->Text = String::Concat(MAJOR_REVISION_NUMBER.ToString(),S".",MINOR_REVISION_NUMBER.ToString(), S".", BUILD_NUMBER.ToString());
+			versionLicensing->Text = String::Concat(MAJOR_REVISION_NUMBER.ToString(),S".",MINOR_REVISION_NUMBER.ToString(), S".", BUILD_NUMBER.ToString());	
 		}
 		System::Void AboutBox_Click(System::Object *  sender, System::EventArgs *  e)
 		{
@@ -83,8 +85,9 @@ namespace KeyReaderGUI
 	protected: System::Windows::Forms::Label *  label1;
 	protected: System::Windows::Forms::Label *  label2;
 	protected: System::Windows::Forms::Label *  label3;
-
-
+	private: System::Windows::Forms::Label*  label4;
+	protected: System::Windows::Forms::Label*  versionServer;
+	
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -97,55 +100,60 @@ namespace KeyReaderGUI
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::Resources::ResourceManager *  resources = new System::Resources::ResourceManager(__typeof(KeyReaderGUI::AboutBox));
-			this->linkLabel1 = new System::Windows::Forms::LinkLabel();
-			this->linkWebsite = new System::Windows::Forms::LinkLabel();
-			this->labelCopyright = new System::Windows::Forms::Label();
-			this->labelRightsReserved = new System::Windows::Forms::Label();
-			this->labelPhoneNumbers = new System::Windows::Forms::Label();
-			this->labelAddress = new System::Windows::Forms::Label();
-			this->labelWebsite = new System::Windows::Forms::Label();
-			this->linkTechSupport = new System::Windows::Forms::LinkLabel();
-			this->linkSales = new System::Windows::Forms::LinkLabel();
-			this->labelFtp = new System::Windows::Forms::Label();
-			this->labelEmail = new System::Windows::Forms::Label();
-			this->labelLicensing = new System::Windows::Forms::Label();
-			this->versionLicensing = new System::Windows::Forms::Label();
-			this->label1 = new System::Windows::Forms::Label();
-			this->label2 = new System::Windows::Forms::Label();
-			this->label3 = new System::Windows::Forms::Label();
+			System::ComponentModel::ComponentResourceManager*  resources = (new System::ComponentModel::ComponentResourceManager(__typeof(AboutBox)));
+			this->linkLabel1 = (new System::Windows::Forms::LinkLabel());
+			this->linkWebsite = (new System::Windows::Forms::LinkLabel());
+			this->labelCopyright = (new System::Windows::Forms::Label());
+			this->labelRightsReserved = (new System::Windows::Forms::Label());
+			this->labelPhoneNumbers = (new System::Windows::Forms::Label());
+			this->labelAddress = (new System::Windows::Forms::Label());
+			this->labelWebsite = (new System::Windows::Forms::Label());
+			this->linkTechSupport = (new System::Windows::Forms::LinkLabel());
+			this->linkSales = (new System::Windows::Forms::LinkLabel());
+			this->labelFtp = (new System::Windows::Forms::Label());
+			this->labelEmail = (new System::Windows::Forms::Label());
+			this->labelLicensing = (new System::Windows::Forms::Label());
+			this->versionLicensing = (new System::Windows::Forms::Label());
+			this->label1 = (new System::Windows::Forms::Label());
+			this->label2 = (new System::Windows::Forms::Label());
+			this->label3 = (new System::Windows::Forms::Label());
+			this->label4 = (new System::Windows::Forms::Label());
+			this->versionServer = (new System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// linkLabel1
 			// 
 			this->linkLabel1->BackColor = System::Drawing::Color::Transparent;
-			this->linkLabel1->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
-			this->linkLabel1->LinkColor = System::Drawing::Color::FromArgb((System::Byte)51, (System::Byte)102, (System::Byte)153);
+			this->linkLabel1->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
+			this->linkLabel1->LinkColor = System::Drawing::Color::FromArgb((System::Int32)(System::Byte)51, (System::Int32)(System::Byte)102, (System::Int32)(System::Byte)153);
 			this->linkLabel1->Location = System::Drawing::Point(416, 376);
 			this->linkLabel1->Name = S"linkLabel1";
 			this->linkLabel1->Size = System::Drawing::Size(208, 16);
 			this->linkLabel1->TabIndex = 22;
 			this->linkLabel1->TabStop = true;
 			this->linkLabel1->Text = S"ftp://downloads.solimarsystems.com";
-			this->linkLabel1->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &KeyReaderGUI::AboutBox::LinkLabel_Click);
+			this->linkLabel1->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &AboutBox::LinkLabel_Click);
 			// 
 			// linkWebsite
 			// 
 			this->linkWebsite->BackColor = System::Drawing::Color::Transparent;
-			this->linkWebsite->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
-			this->linkWebsite->LinkColor = System::Drawing::Color::FromArgb((System::Byte)51, (System::Byte)102, (System::Byte)153);
+			this->linkWebsite->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
+			this->linkWebsite->LinkColor = System::Drawing::Color::FromArgb((System::Int32)(System::Byte)51, (System::Int32)(System::Byte)102, (System::Int32)(System::Byte)153);
 			this->linkWebsite->Location = System::Drawing::Point(416, 360);
 			this->linkWebsite->Name = S"linkWebsite";
 			this->linkWebsite->Size = System::Drawing::Size(208, 16);
 			this->linkWebsite->TabIndex = 19;
 			this->linkWebsite->TabStop = true;
 			this->linkWebsite->Text = S"http://www.solimarsystems.com";
-			this->linkWebsite->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &KeyReaderGUI::AboutBox::LinkLabel_Click);
+			this->linkWebsite->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &AboutBox::LinkLabel_Click);
 			// 
 			// labelCopyright
 			// 
 			this->labelCopyright->BackColor = System::Drawing::Color::Transparent;
-			this->labelCopyright->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelCopyright->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelCopyright->Location = System::Drawing::Point(208, 484);
 			this->labelCopyright->Name = S"labelCopyright";
 			this->labelCopyright->Size = System::Drawing::Size(384, 14);
@@ -155,7 +163,8 @@ namespace KeyReaderGUI
 			// labelRightsReserved
 			// 
 			this->labelRightsReserved->BackColor = System::Drawing::Color::Transparent;
-			this->labelRightsReserved->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelRightsReserved->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelRightsReserved->Location = System::Drawing::Point(208, 500);
 			this->labelRightsReserved->Name = S"labelRightsReserved";
 			this->labelRightsReserved->Size = System::Drawing::Size(384, 16);
@@ -165,7 +174,8 @@ namespace KeyReaderGUI
 			// labelPhoneNumbers
 			// 
 			this->labelPhoneNumbers->BackColor = System::Drawing::Color::Transparent;
-			this->labelPhoneNumbers->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelPhoneNumbers->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelPhoneNumbers->Location = System::Drawing::Point(208, 392);
 			this->labelPhoneNumbers->Name = S"labelPhoneNumbers";
 			this->labelPhoneNumbers->Size = System::Drawing::Size(148, 16);
@@ -175,7 +185,8 @@ namespace KeyReaderGUI
 			// labelAddress
 			// 
 			this->labelAddress->BackColor = System::Drawing::Color::Transparent;
-			this->labelAddress->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelAddress->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelAddress->Location = System::Drawing::Point(208, 344);
 			this->labelAddress->Name = S"labelAddress";
 			this->labelAddress->Size = System::Drawing::Size(148, 16);
@@ -185,7 +196,8 @@ namespace KeyReaderGUI
 			// labelWebsite
 			// 
 			this->labelWebsite->BackColor = System::Drawing::Color::Transparent;
-			this->labelWebsite->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelWebsite->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelWebsite->Location = System::Drawing::Point(372, 360);
 			this->labelWebsite->Name = S"labelWebsite";
 			this->labelWebsite->Size = System::Drawing::Size(42, 16);
@@ -195,33 +207,37 @@ namespace KeyReaderGUI
 			// linkTechSupport
 			// 
 			this->linkTechSupport->BackColor = System::Drawing::Color::Transparent;
-			this->linkTechSupport->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
-			this->linkTechSupport->LinkColor = System::Drawing::Color::FromArgb((System::Byte)51, (System::Byte)102, (System::Byte)153);
+			this->linkTechSupport->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
+			this->linkTechSupport->LinkColor = System::Drawing::Color::FromArgb((System::Int32)(System::Byte)51, (System::Int32)(System::Byte)102, 
+				(System::Int32)(System::Byte)153);
 			this->linkTechSupport->Location = System::Drawing::Point(416, 392);
 			this->linkTechSupport->Name = S"linkTechSupport";
 			this->linkTechSupport->Size = System::Drawing::Size(208, 16);
 			this->linkTechSupport->TabIndex = 20;
 			this->linkTechSupport->TabStop = true;
 			this->linkTechSupport->Text = S"tech.support@solimarsystems.com";
-			this->linkTechSupport->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &KeyReaderGUI::AboutBox::EmailLink_Click);
+			this->linkTechSupport->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &AboutBox::EmailLink_Click);
 			// 
 			// linkSales
 			// 
 			this->linkSales->BackColor = System::Drawing::Color::Transparent;
-			this->linkSales->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
-			this->linkSales->LinkColor = System::Drawing::Color::FromArgb((System::Byte)51, (System::Byte)102, (System::Byte)153);
+			this->linkSales->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
+			this->linkSales->LinkColor = System::Drawing::Color::FromArgb((System::Int32)(System::Byte)51, (System::Int32)(System::Byte)102, (System::Int32)(System::Byte)153);
 			this->linkSales->Location = System::Drawing::Point(416, 408);
 			this->linkSales->Name = S"linkSales";
 			this->linkSales->Size = System::Drawing::Size(208, 16);
 			this->linkSales->TabIndex = 21;
 			this->linkSales->TabStop = true;
 			this->linkSales->Text = S"sales@solimarsystems.com";
-			this->linkSales->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &KeyReaderGUI::AboutBox::EmailLink_Click);
+			this->linkSales->LinkClicked += new System::Windows::Forms::LinkLabelLinkClickedEventHandler(this, &AboutBox::EmailLink_Click);
 			// 
 			// labelFtp
 			// 
 			this->labelFtp->BackColor = System::Drawing::Color::Transparent;
-			this->labelFtp->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelFtp->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelFtp->Location = System::Drawing::Point(372, 376);
 			this->labelFtp->Name = S"labelFtp";
 			this->labelFtp->Size = System::Drawing::Size(42, 16);
@@ -231,7 +247,8 @@ namespace KeyReaderGUI
 			// labelEmail
 			// 
 			this->labelEmail->BackColor = System::Drawing::Color::Transparent;
-			this->labelEmail->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelEmail->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelEmail->Location = System::Drawing::Point(372, 392);
 			this->labelEmail->Name = S"labelEmail";
 			this->labelEmail->Size = System::Drawing::Size(42, 16);
@@ -241,7 +258,8 @@ namespace KeyReaderGUI
 			// labelLicensing
 			// 
 			this->labelLicensing->BackColor = System::Drawing::Color::Transparent;
-			this->labelLicensing->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->labelLicensing->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->labelLicensing->Location = System::Drawing::Point(208, 436);
 			this->labelLicensing->Name = S"labelLicensing";
 			this->labelLicensing->Size = System::Drawing::Size(108, 16);
@@ -251,7 +269,8 @@ namespace KeyReaderGUI
 			// versionLicensing
 			// 
 			this->versionLicensing->BackColor = System::Drawing::Color::Transparent;
-			this->versionLicensing->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->versionLicensing->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->versionLicensing->Location = System::Drawing::Point(208, 452);
 			this->versionLicensing->Name = S"versionLicensing";
 			this->versionLicensing->Size = System::Drawing::Size(108, 16);
@@ -261,7 +280,8 @@ namespace KeyReaderGUI
 			// label1
 			// 
 			this->label1->BackColor = System::Drawing::Color::Transparent;
-			this->label1->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->label1->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->label1->Location = System::Drawing::Point(208, 360);
 			this->label1->Name = S"label1";
 			this->label1->Size = System::Drawing::Size(148, 16);
@@ -271,7 +291,8 @@ namespace KeyReaderGUI
 			// label2
 			// 
 			this->label2->BackColor = System::Drawing::Color::Transparent;
-			this->label2->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->label2->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->label2->Location = System::Drawing::Point(208, 376);
 			this->label2->Name = S"label2";
 			this->label2->Size = System::Drawing::Size(148, 16);
@@ -281,18 +302,43 @@ namespace KeyReaderGUI
 			// label3
 			// 
 			this->label3->BackColor = System::Drawing::Color::Transparent;
-			this->label3->Font = new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, (System::Byte)0);
+			this->label3->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
 			this->label3->Location = System::Drawing::Point(208, 408);
 			this->label3->Name = S"label3";
 			this->label3->Size = System::Drawing::Size(148, 16);
 			this->label3->TabIndex = 14;
 			this->label3->Text = S"Fax:  +1.619.849.2801";
 			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
+			this->label4->Location = System::Drawing::Point(372, 437);
+			this->label4->Name = S"label4";
+			this->label4->Size = System::Drawing::Size(48, 15);
+			this->label4->TabIndex = 23;
+			this->label4->Text = S"Server:";
+			// 
+			// versionServer
+			// 
+			this->versionServer->BackColor = System::Drawing::Color::Transparent;
+			this->versionServer->Font = (new System::Drawing::Font(S"Arial", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				(System::Byte)0));
+			this->versionServer->Location = System::Drawing::Point(372, 452);
+			this->versionServer->Name = S"versionServer";
+			this->versionServer->Size = System::Drawing::Size(108, 16);
+			this->versionServer->TabIndex = 24;
+			this->versionServer->Text = S"XX.XX.XXXXXX";
+			// 
 			// AboutBox
 			// 
 			this->AutoScaleBaseSize = System::Drawing::Size(5, 13);
-			this->BackgroundImage = (__try_cast<System::Drawing::Image *  >(resources->GetObject(S"$this.BackgroundImage")));
+			this->BackgroundImage = (__try_cast<System::Drawing::Image*  >(resources->GetObject(S"$this.BackgroundImage")));
 			this->ClientSize = System::Drawing::Size(736, 560);
+			this->Controls->Add(this->versionServer);
+			this->Controls->Add(this->label4);
 			this->Controls->Add(this->linkLabel1);
 			this->Controls->Add(this->linkWebsite);
 			this->Controls->Add(this->labelCopyright);
@@ -312,11 +358,13 @@ namespace KeyReaderGUI
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = S"AboutBox";
 			this->Text = S"AboutBox";
-			this->Click += new System::EventHandler(this, &KeyReaderGUI::AboutBox::AboutBox_Click);
-			this->Load += new System::EventHandler(this, &KeyReaderGUI::AboutBox::AboutBox_Load);
+			this->Click += new System::EventHandler(this, &AboutBox::AboutBox_Click);
+			this->Load += new System::EventHandler(this, &AboutBox::AboutBox_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}		
+
 
 
 
