c6f69bb92b7458931ae7f5809f6931af401172cd
Alan Chou <alan.chou@solimarsystems.com>
2007-10-15 09:29:40 -0700

    changed error message wording Prev Revisions: added check for enter/return keys after filling password edit box

--- CreatorCode/SolimarLicensing/KeyReaderGUI/PswdForm.cpp
+++ CreatorCode/SolimarLicensing/KeyReaderGUI/PswdForm.cpp
@@ -20,13 +20,15 @@ PswdForm::~PswdForm()
 System::Void PswdForm::OkBtn_Click(System::Object *  sender, System::EventArgs *  e)
 {
 	String* InputString = this->PswdTextBox->Text; //this->Controls->get_Item(0)->Text;
+	System::Windows::Forms::Cursor* storedCursor = this->Cursor;
+	this->Cursor = Cursors::WaitCursor;	
 	HRESULT hr = PasswordValidater->CheckPassword(InputString);
-
+	this->Cursor = storedCursor;
 #undef MessageBox
 	//The protocol is S_FALSE signifies invalid password
 	if(hr == S_FALSE)
 	{
-		System::Windows::Forms::MessageBox::Show("Invalid Password");
+		System::Windows::Forms::MessageBox::Show("Invalid Password", "Password Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 	}
 	//S_OK signifies correct password
 	else if(hr == S_OK)
@@ -36,7 +38,7 @@ System::Void PswdForm::OkBtn_Click(System::Object *  sender, System::EventArgs *
 	//else the function failed
 	else
 	{
-		System::Windows::Forms::MessageBox::Show("Function Call Failed");
+		System::Windows::Forms::MessageBox::Show("Failed Trying to Validate Password", "Password Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 	}
 	this->Close();
 }
@@ -64,6 +66,14 @@ System::Void PswdForm::PswdTextBox_Validating(System::Object *  sender, System::
                 // Set the ErrorProvider error with the text to display.
 	    PswdErrorProvider->SetError(this->PswdTextBox, errorMsg);
     }
+}
+
+System::Void PswdForm::PswdTextBox_KeyDown(System::Object*  sender, System::Windows::Forms::KeyEventArgs*  e)
+{
+	if (e->KeyCode == Keys::Enter || e->KeyCode == Keys::Return)
+		OkBtn_Click(sender,e);	
+	else if (e->KeyCode == Keys::Escape)
+		DialogResult = DialogResult::Cancel;
 }
 
 bool PswdForm::IsValidPassword(String* password,  String** errorMessage) 
