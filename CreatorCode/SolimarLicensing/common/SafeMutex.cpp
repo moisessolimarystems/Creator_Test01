4a8e2f42dd6630a7d425cbad1287d9244be794f3
jlan <jeff.lan@solimarsystems.com>
2007-10-18 09:04:20 -0700

    Solimar Licensing - License Server - Improved stability in the license server. There was an uninitialized mutex handle and an object leak with the protection keys fixed. Both of these fixes helped with the occasional deadlock problem.

--- CreatorCode/SolimarLicensing/common/SafeMutex.cpp
+++ CreatorCode/SolimarLicensing/common/SafeMutex.cpp
@@ -1,12 +1,95 @@
 #include "SafeMutex.h"
+#include <stdio.h>
 
-SafeMutex::SafeMutex(HANDLE mutexHandle)
+SafeMutex::SafeMutex(HANDLE hMutex, DWORD dwTo)
 {
-	m_mutex=mutexHandle;
-	WaitForSingleObject(m_mutex, INFINITE);
-}
+   THandleList hlMutexList ;
 
+   hlMutexList.push_back(hMutex) ;
+   _init(hlMutexList, dwTo) ;
+}
+SafeMutex::SafeMutex(const THandleList &hlMutexList, DWORD dwTo)
+{
+   _init(hlMutexList, dwTo) ;
+}
 SafeMutex::~SafeMutex()
 {
-	ReleaseMutex(m_mutex);
+   TItHandleList ithMutex ;
+
+   for (
+      ithMutex = m_hlMutexList.begin() ;
+      ithMutex != m_hlMutexList.end() ;
+      ithMutex++
+      )
+   {
+      HANDLE hMutex(*ithMutex) ;
+
+      ReleaseMutex(hMutex) ;
+   }
+}
+
+bool SafeMutex::Locked() 
+{
+   return m_bSuccess ; 
+}
+
+void SafeMutex::_init(const THandleList &hlMutexList, DWORD dwTo) 
+{
+   TItcHandleList ithMutex ;
+   ULONG ulWfsoResult(WAIT_OBJECT_0) ;   
+   //
+   // initialize state members
+   //
+   m_bSuccess = false ;
+   //
+   // gain ownership of mutexi
+   //
+   for (
+      ithMutex = hlMutexList.begin() ;
+      ithMutex != hlMutexList.end() && ulWfsoResult==WAIT_OBJECT_0;
+      ithMutex++
+      )
+   {
+      HANDLE hMutex(*ithMutex) ;
+
+      if (hMutex==INVALID_HANDLE_VALUE)
+      {  //
+         // problem: not a valid mutex handle.
+         // Note,  WaitForSingleObject() does not treat INVALID_HANDLE_VALUE as invalid... 
+         // it will wait for ever...  Hence catch it here.
+         //
+         assert(hMutex!=INVALID_HANDLE_VALUE) ;
+      }
+      else
+      {  
+         ulWfsoResult = WaitForSingleObject(hMutex, dwTo) ;
+         if (ulWfsoResult==WAIT_FAILED)
+         {  
+            DWORD dwNtErr(ERROR_SUCCESS) ;
+            //
+            // problem: WaitForSingleObject() failed.
+            //
+            dwNtErr = GetLastError() ;
+            assert(ulWfsoResult!=WAIT_FAILED) ;
+         }
+         else if (ulWfsoResult==WAIT_TIMEOUT)
+         {  //
+            // problem: did not get ownership of object.
+            //
+            break ;
+         }
+         else if (ulWfsoResult==WAIT_OBJECT_0)
+         {  //
+            // Successfully obtained ownership of the mutex.
+            // Insert the mutex into the list that the destructor
+            // will release.
+            //
+            m_hlMutexList.push_back(hMutex) ;
+         }
+      }
+   }
+   if (ithMutex == hlMutexList.end())
+   {
+      m_bSuccess = true ;
+   }
 }
