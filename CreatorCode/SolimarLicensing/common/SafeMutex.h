a6895f5e45a929d725f6680670d9c812fb4c51f7
jlan <jeff.lan@solimarsystems.com>
2007-10-18 09:04:20 -0700

    Solimar Licensing - License Server - Improved stability in the license server. There was an uninitialized mutex handle and an object leak with the protection keys fixed. Both of these fixes helped with the occasional deadlock problem.

--- CreatorCode/SolimarLicensing/common/SafeMutex.h
+++ CreatorCode/SolimarLicensing/common/SafeMutex.h
@@ -1,15 +1,29 @@
 #ifndef __SafeMutex_H
 #define __SafeMutex_H
 
+// smart pointer class which handles mutex blocking/releasing.
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <vector>
+#include <assert.h>
 
 class SafeMutex
 {
 public:
-	SafeMutex(HANDLE mutexHandle);
-	~SafeMutex();
+   typedef std::vector<HANDLE> THandleList ;
+   typedef THandleList::iterator TItHandleList ;
+   typedef THandleList::const_iterator TItcHandleList ;
 private:
-	HANDLE m_mutex;
-};
+   THandleList m_hlMutexList ;
+   bool m_bSuccess ;
+public:
+   SafeMutex(HANDLE hMutex, DWORD dwTo=INFINITE);
+   SafeMutex(const THandleList &hlMutexList, DWORD dwTo=INFINITE);
+   ~SafeMutex();
+   bool Locked();
+   
+private:
+   void _init(const THandleList &hlMutexList, DWORD dwTo=INFINITE);
+} ;
 
 #endif
