295e296f26a776d4fc03dc386f867ca5a2b7871d
jlan <jeff.lan@solimarsystems.com>
2008-10-24 01:47:05 -0700

    SoftwareLicense.v48 - Solimar Licensing - Lincense Manager - ConnectByProducts() - Fixed the issue where reading the Test/Dev from the license settings was establishing the product connection. Prev Revisions: Initial changes for software licensing.

--- CreatorCode/SolimarLicensing/common/LicenseSettings.h
+++ CreatorCode/SolimarLicensing/common/LicenseSettings.h
@@ -9,13 +9,38 @@
 class LicenseSettings
 {
 	public:
-		class LicenseServerSettings
+		class LicenseServerSettingsOnePointZero
 		{
 			public :
-				LicenseServerSettings(): serverName(L""), bIsBackupServer(false), bIsSharedServer(false){}
+				LicenseServerSettingsOnePointZero(): 
+					 serverName(L""), 
+					 bIsBackupServer(false), 
+					 bIsSharedServer(false),
+					 bIsSoftwareLicensing(false),
+					 bIsTestDevLicensing(false),
+					 bIsDisasterRecoveryServer(false)
+					 {}
+
 				_bstr_t serverName;
 				bool bIsBackupServer;
 				bool bIsSharedServer;
+				bool bIsSoftwareLicensing;
+				bool bIsTestDevLicensing;
+				bool bIsDisasterRecoveryServer;
+		};
+
+		class LicenseServerSettingsTwoPointZero
+		{
+			public :
+				LicenseServerSettingsTwoPointZero(): 
+					 serverName(L""), 
+					 backupServerName(L""), 
+					 bIsTestDevLicensing(false)
+					 {}
+
+				_bstr_t serverName;
+				_bstr_t backupServerName;
+				bool bIsTestDevLicensing;
 		};
 
 		LicenseSettings(): m_bstrXmlSettingsVersion(L""), m_xmlReaderMutex(CreateMutex(0, 0, L"")){}
@@ -25,10 +50,13 @@ class LicenseSettings
 				CloseHandle(m_xmlReaderMutex);
 		}
 		HRESULT Initialize();
-		HRESULT GetLiceseServerByProduct(long productID, std::list<LicenseServerSettings*>* pLicenseServerList);
+		HRESULT GetLiceseServerByProduct(long productID, LicenseServerSettingsTwoPointZero* pLicenseServer);
 
 	private:
 		HANDLE m_xmlReaderMutex; 
 		XMLConfig m_xmlReader;
 		_bstr_t m_bstrXmlSettingsVersion;
+
+		HRESULT LoadVersionOnePointZeroXML(long productID, std::list<LicenseServerSettingsOnePointZero*>* pLicenseServerList);
+		HRESULT LoadVersionTwoPointZeroXML(long productID, LicenseServerSettingsTwoPointZero* pLicenseServer);
 };
