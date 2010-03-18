dc32275753f288f02f589e37ad4bd458175ddba2
mksuser <systems@solimarsystems.com>
2010-03-18 04:02:54 -0700

    updated to include vs7,7.1 in define. Prev Revisions: CR.FIX.12632.Licensing - Solimar Licensing - Licensing 3.0 - Added more message ID's to the messages that the license server sends to it's clients. Also log the message ID to the event log when logging messages. added defines for visual studio 6.0 for swprintf_s.

--- CreatorCode/SolimarLicensing/common/EventLogHelper.h
+++ CreatorCode/SolimarLicensing/common/EventLogHelper.h
@@ -3,11 +3,25 @@
 
 #include <comdef.h>
 #include <tchar.h>
+// vc6 = 1200, vc7 = 1300, vc7.1 = 1310, vc8 = 1400
+#if _MSC_VER < 1400
+#include <stdio.h>
+#define swprintf_s _snwprintf
+#endif
 
 class EventLogHelper
 {
 public:
-    static HRESULT WriteEventLog(wchar_t* event_source, wchar_t *event_log_msg, unsigned int event_type);
+    static HRESULT WriteEventLog(wchar_t* event_source, wchar_t *event_log_msg, unsigned int event_type, long event_id);
+    static HRESULT ReadEventLog_Helper();
+//	 static LPWSTR GetMessageString(DWORD MessageId, DWORD argc, LPWSTR argv);
+private:
+static DWORD DumpRecordsInBuffer(PBYTE pBuffer, DWORD dwBytesRead);
+static DWORD GetEventTypeName(DWORD EventType);
+static LPWSTR GetMessageString(DWORD Id, DWORD argc, LPWSTR args);
+static void GetTimestamp(const DWORD Time, WCHAR DisplayString[]);
+static DWORD ApplyParameterStringsToMessage(CONST LPCWSTR pMessage, LPWSTR & pFinalMessage);
+
 };
 
 #endif
