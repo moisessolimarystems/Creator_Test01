b1e3b184bbbded79da9bbae74d1b788d594ebda9
jlan <jeff.lan@solimarsystems.com>
2010-05-27 09:44:36 -0700

    SoftwareLicense.v218 - Solimar Licensing - Diagnostic Data Viewer - When viewing ValidationTokenRetriever Data, Changed UI to display all of the tokens.

--- CreatorCode/SolimarLicensing/SolimarLicenseDiagnosticDataViewer/SolimarValidationToken_DisplayForm.Designer.cs
+++ CreatorCode/SolimarLicensing/SolimarLicenseDiagnosticDataViewer/SolimarValidationToken_DisplayForm.Designer.cs
@@ -29,148 +29,76 @@
         private void InitializeComponent()
         {
             this.mainBackPanel = new System.Windows.Forms.Panel();
-            this.uuidLabel = new System.Windows.Forms.Label();
-            this.uuidTextBox = new System.Windows.Forms.TextBox();
-            this.companyLabel = new System.Windows.Forms.Label();
-            this.companyTextBox = new System.Windows.Forms.TextBox();
-            this.biosLabel = new System.Windows.Forms.Label();
-            this.macLabel = new System.Windows.Forms.Label();
-            this.compNameLabel = new System.Windows.Forms.Label();
-            this.biosTextBox = new System.Windows.Forms.TextBox();
-            this.macTextBox = new System.Windows.Forms.TextBox();
-            this.compNameTextBox = new System.Windows.Forms.TextBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.tokenRetrieverTitleLabel = new System.Windows.Forms.Label();
+            this.tokenRetrieverLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.mainBackPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainBackPanel
             // 
-            this.mainBackPanel.Controls.Add(this.uuidLabel);
-            this.mainBackPanel.Controls.Add(this.uuidTextBox);
-            this.mainBackPanel.Controls.Add(this.companyLabel);
-            this.mainBackPanel.Controls.Add(this.companyTextBox);
-            this.mainBackPanel.Controls.Add(this.biosLabel);
-            this.mainBackPanel.Controls.Add(this.macLabel);
-            this.mainBackPanel.Controls.Add(this.compNameLabel);
-            this.mainBackPanel.Controls.Add(this.biosTextBox);
-            this.mainBackPanel.Controls.Add(this.macTextBox);
-            this.mainBackPanel.Controls.Add(this.compNameTextBox);
+            this.mainBackPanel.Controls.Add(this.propertyGrid1);
+            this.mainBackPanel.Controls.Add(this.panel1);
             this.mainBackPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainBackPanel.Location = new System.Drawing.Point(0, 0);
             this.mainBackPanel.Name = "mainBackPanel";
-            this.mainBackPanel.Size = new System.Drawing.Size(502, 160);
+            this.mainBackPanel.Size = new System.Drawing.Size(583, 252);
             this.mainBackPanel.TabIndex = 2;
             // 
-            // uuidLabel
+            // propertyGrid1
             // 
-            this.uuidLabel.AutoSize = true;
-            this.uuidLabel.Location = new System.Drawing.Point(12, 122);
-            this.uuidLabel.Name = "uuidLabel";
-            this.uuidLabel.Size = new System.Drawing.Size(114, 13);
-            this.uuidLabel.TabIndex = 12;
-            this.uuidLabel.Text = "System Product UUID:";
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid1.HelpVisible = false;
+            this.propertyGrid1.Location = new System.Drawing.Point(0, 32);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.PropertySort = System.Windows.Forms.PropertySort.NoSort;
+            this.propertyGrid1.Size = new System.Drawing.Size(583, 220);
+            this.propertyGrid1.TabIndex = 13;
+            this.propertyGrid1.ToolbarVisible = false;
+            this.propertyGrid1.ViewForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             // 
-            // uuidTextBox
+            // tokenRetrieverTitleLabel
             // 
-            this.uuidTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.uuidTextBox.Location = new System.Drawing.Point(146, 119);
-            this.uuidTextBox.Name = "uuidTextBox";
-            this.uuidTextBox.ReadOnly = true;
-            this.uuidTextBox.Size = new System.Drawing.Size(344, 20);
-            this.uuidTextBox.TabIndex = 11;
-            this.uuidTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GeneralMouseDown);
+            this.tokenRetrieverTitleLabel.AutoSize = true;
+            this.tokenRetrieverTitleLabel.Location = new System.Drawing.Point(3, 9);
+            this.tokenRetrieverTitleLabel.Name = "tokenRetrieverTitleLabel";
+            this.tokenRetrieverTitleLabel.Size = new System.Drawing.Size(125, 13);
+            this.tokenRetrieverTitleLabel.TabIndex = 14;
+            this.tokenRetrieverTitleLabel.Text = "Token Retreiver Version:";
             // 
-            // companyLabel
+            // tokenRetrieverLabel
             // 
-            this.companyLabel.AutoSize = true;
-            this.companyLabel.Location = new System.Drawing.Point(12, 19);
-            this.companyLabel.Name = "companyLabel";
-            this.companyLabel.Size = new System.Drawing.Size(85, 13);
-            this.companyLabel.TabIndex = 10;
-            this.companyLabel.Text = "Company Name:";
+            this.tokenRetrieverLabel.AutoSize = true;
+            this.tokenRetrieverLabel.Location = new System.Drawing.Point(134, 9);
+            this.tokenRetrieverLabel.Name = "tokenRetrieverLabel";
+            this.tokenRetrieverLabel.Size = new System.Drawing.Size(94, 13);
+            this.tokenRetrieverLabel.TabIndex = 15;
+            this.tokenRetrieverLabel.Text = "[Retriever Version]";
             // 
-            // companyTextBox
+            // panel1
             // 
-            this.companyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.companyTextBox.Location = new System.Drawing.Point(146, 16);
-            this.companyTextBox.Name = "companyTextBox";
-            this.companyTextBox.ReadOnly = true;
-            this.companyTextBox.Size = new System.Drawing.Size(344, 20);
-            this.companyTextBox.TabIndex = 0;
-            this.companyTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GeneralMouseDown);
-            // 
-            // biosLabel
-            // 
-            this.biosLabel.AutoSize = true;
-            this.biosLabel.Location = new System.Drawing.Point(12, 96);
-            this.biosLabel.Name = "biosLabel";
-            this.biosLabel.Size = new System.Drawing.Size(99, 13);
-            this.biosLabel.TabIndex = 6;
-            this.biosLabel.Text = "Bios Serial Number:";
-            // 
-            // macLabel
-            // 
-            this.macLabel.AutoSize = true;
-            this.macLabel.Location = new System.Drawing.Point(12, 70);
-            this.macLabel.Name = "macLabel";
-            this.macLabel.Size = new System.Drawing.Size(74, 13);
-            this.macLabel.TabIndex = 5;
-            this.macLabel.Text = "MAC Address:";
-            // 
-            // compNameLabel
-            // 
-            this.compNameLabel.AutoSize = true;
-            this.compNameLabel.Location = new System.Drawing.Point(12, 44);
-            this.compNameLabel.Name = "compNameLabel";
-            this.compNameLabel.Size = new System.Drawing.Size(86, 13);
-            this.compNameLabel.TabIndex = 4;
-            this.compNameLabel.Text = "Computer Name:";
-            // 
-            // biosTextBox
-            // 
-            this.biosTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.biosTextBox.Location = new System.Drawing.Point(146, 93);
-            this.biosTextBox.Name = "biosTextBox";
-            this.biosTextBox.ReadOnly = true;
-            this.biosTextBox.Size = new System.Drawing.Size(344, 20);
-            this.biosTextBox.TabIndex = 3;
-            this.biosTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GeneralMouseDown);
-            // 
-            // macTextBox
-            // 
-            this.macTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.macTextBox.Location = new System.Drawing.Point(146, 67);
-            this.macTextBox.Name = "macTextBox";
-            this.macTextBox.ReadOnly = true;
-            this.macTextBox.Size = new System.Drawing.Size(344, 20);
-            this.macTextBox.TabIndex = 2;
-            this.macTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GeneralMouseDown);
-            // 
-            // compNameTextBox
-            // 
-            this.compNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.compNameTextBox.Location = new System.Drawing.Point(146, 41);
-            this.compNameTextBox.Name = "compNameTextBox";
-            this.compNameTextBox.ReadOnly = true;
-            this.compNameTextBox.Size = new System.Drawing.Size(344, 20);
-            this.compNameTextBox.TabIndex = 1;
-            this.compNameTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.GeneralMouseDown);
+            this.panel1.Controls.Add(this.tokenRetrieverTitleLabel);
+            this.panel1.Controls.Add(this.tokenRetrieverLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 32);
+            this.panel1.TabIndex = 16;
             // 
             // SolimarValidationToken_DisplayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 160);
+            this.ClientSize = new System.Drawing.Size(583, 252);
             this.Controls.Add(this.mainBackPanel);
             this.Name = "SolimarValidationToken_DisplayForm";
             this.ShowIcon = false;
             this.Text = "Solimar Validation Token";
             this.mainBackPanel.ResumeLayout(false);
-            this.mainBackPanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -178,15 +106,9 @@
         #endregion
 
         private System.Windows.Forms.Panel mainBackPanel;
-        private System.Windows.Forms.Label uuidLabel;
-        private System.Windows.Forms.TextBox uuidTextBox;
-        private System.Windows.Forms.Label companyLabel;
-        private System.Windows.Forms.TextBox companyTextBox;
-        private System.Windows.Forms.Label biosLabel;
-        private System.Windows.Forms.Label macLabel;
-        private System.Windows.Forms.Label compNameLabel;
-        private System.Windows.Forms.TextBox biosTextBox;
-        private System.Windows.Forms.TextBox macTextBox;
-        private System.Windows.Forms.TextBox compNameTextBox;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.Label tokenRetrieverLabel;
+        private System.Windows.Forms.Label tokenRetrieverTitleLabel;
+        private System.Windows.Forms.Panel panel1;
     }
 }
