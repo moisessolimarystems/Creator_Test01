1aa0cd14035a9a4edc99080d613998edf39a2ac3
mksuser <systems@solimarsystems.com>
2010-08-06 08:31:10 -0700

    build_Creator3.0.181

---
+++ CreatorCode/CreatorClient/ProductLicenseSelectionForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Client.Creator
+{
+    partial class ProductLicenseSelectionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(86, 5);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(5, 5);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(462, 238);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Product License Number";
+            this.columnHeader1.Width = 142;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Type";
+            this.columnHeader2.Width = 61;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Product";
+            this.columnHeader3.Width = 75;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Exp Date";
+            this.columnHeader4.Width = 83;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Description";
+            this.columnHeader5.Width = 103;
+            // 
+            // ProductLicenseSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(486, 287);
+            this.Controls.Add(this.listView1);
+            this.Name = "ProductLicenseSelectionForm";
+            this.Text = "Product License Selection";
+            this.FinishDialog += new Shared.VisualComponents.FinishDialogEventHandler(this.ProductLicenseSelectionForm_FinishDialog);
+            this.InitDialog += new Shared.VisualComponents.InitDialogEventHandler(this.ProductLicenseSelectionForm_InitDialog);
+            this.Controls.SetChildIndex(this.listView1, 0);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+
+    }
+}
---
+++ CreatorCode/CreatorClient/ProductLicenseSelectionForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Client.Creator.CreatorService;
+using Solimar.Licensing.Attribs;
+using Client.Creator.ServiceProxy;
+
+namespace Client.Creator
+{
+    public partial class ProductLicenseSelectionForm : Shared.VisualComponents.DialogBaseForm
+    {
+        private ListViewMgr _lvManager;
+        private CommunicationLink _commLink;
+        public ProductLicenseSelectionForm(CommunicationLink commLink)
+        {
+            InitializeComponent();
+            _lvManager = new ListViewMgr();
+            _lvManager.SetListViewColumnSorter(listView1);
+            _commLink = commLink;
+        }
+
+        private void ProductLicenseSelectionForm_InitDialog(object sender, Shared.VisualComponents.InitDialogEventArgs e)
+        {
+            LicenseServerProperty licData = (e.Data as ProductLicenseSelectionData).SelectedLicense;
+            Service<ICreator>.Use((client) =>
+            {
+                string licenseName = Lic_LicenseInfoAttribsHelper.GenerateLicenseServerName((int)licData.CustID, (int)licData.DestID, (int)licData.GroupID);
+                List<ProductLicenseTable> productLicenses = client.GetProductLicenses(licenseName, false);
+                List<ProductLicenseTable> subProductLicenses = client.GetProductLicenses(licData.Name, false);
+                foreach (ProductLicenseTable plRecord in productLicenses)
+                {
+                    if (plRecord.plState != (byte)ProductLicenseState.AddOn && plRecord.IsActive)
+                    {
+                        //if order already exists apply check to checkbox and make greyed out? unselectable?
+                        ListViewItem lvItem = new ListViewItem();
+                        lvItem.Text = plRecord.plID;
+                        lvItem.SubItems.Add(Enum.GetName(typeof(ProductLicenseState), plRecord.plState));
+                        lvItem.SubItems.Add(_commLink.GetProductName(plRecord.ProductID));
+                        if (plRecord.ExpirationDate.HasValue)
+                            lvItem.SubItems.Add(plRecord.ExpirationDate.Value.ToLocalTime().ToShortDateString());
+                        else
+                            lvItem.SubItems.Add("");
+                        lvItem.SubItems.Add(plRecord.Description);
+                        listView1.Items.Add(lvItem);
+                    }
+                }
+                _lvManager.AutoResizeColumns(listView1);
+            });
+        }
+
+        private void ProductLicenseSelectionForm_FinishDialog(object sender, Shared.VisualComponents.FinishDialogEventArgs e)
+        {
+            foreach (ListViewItem lvItem in listView1.Items)
+            {
+                if (lvItem.Checked)
+                    (e.Data as ProductLicenseSelectionData).ProductLicenseSelectionList.Add(lvItem.Text);
+            }
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            _lvManager.SetSortIndexColumn(listView1.Handle, e.Column);
+            // Perform the sort with these new sort options.
+            listView1.Sort();
+        }
+
+    }
+    #region ProductLicenseSelectionData class
+    public class ProductLicenseSelectionData : Shared.VisualComponents.DialogData
+    {
+        private LicenseServerProperty _selectedLicense;
+        private List<string> _productLicenseSelectionList;
+        #region Constructors
+
+        public ProductLicenseSelectionData(LicenseServerProperty selectedLicense)
+        {
+            _selectedLicense = selectedLicense;
+            _productLicenseSelectionList = new List<string>();
+        }
+        #endregion
+
+        #region Properties
+
+        public LicenseServerProperty SelectedLicense
+        {
+            get { return _selectedLicense; }
+            set { _selectedLicense = value; }
+        }
+
+        public List<string> ProductLicenseSelectionList
+        {
+            get { return _productLicenseSelectionList; }
+            set { _productLicenseSelectionList = value; }
+        }        
+        #endregion
+    }
+    #endregion
+}
