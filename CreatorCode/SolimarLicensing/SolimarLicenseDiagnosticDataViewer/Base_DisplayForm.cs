cdbb8f553d49af4e52ddbd56c48f54793ac161ec
jlan <jeff.lan@solimarsystems.com>
2010-09-10 02:43:42 -0700

    Initial revision

--- CreatorCode/SolimarLicensing/SolimarLicenseDiagnosticDataViewer/Base_DisplayForm.cs
+++ CreatorCode/SolimarLicensing/SolimarLicenseDiagnosticDataViewer/Base_DisplayForm.cs
@@ -6,12 +6,8 @@ using System.Windows.Forms;
 
 namespace SolimarLicenseDiagnosticDataViewer
 {
-    public class Base_DisplayForm : Form
+    abstract public class Base_DisplayForm : Form
     {
-        public Base_DisplayForm()
-        {
-            InitializeComponent();
-        }
         private const int CP_NOCLOSE_BUTTON = 0x200;
         protected override CreateParams CreateParams
         {
@@ -22,46 +18,25 @@ namespace SolimarLicenseDiagnosticDataViewer
                 return cp;
             }
         }
-
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // Base_DisplayForm
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 264);
-            this.Name = "Base_DisplayForm";
-            this.ShowIcon = false;
-            this.ResumeLayout(false);
-
-        }
     }
 
-    //public class Base_DisplayForm<T> : Base_DisplayForm, ISetData<T>
-    //{
-    //    public Base_DisplayForm()
-    //        : base()
-    //    {
-    //    }
-    //    //Implements ISetData<T>
-    //    virtual public void SetData(T t) { }
-    //}
+    abstract public class Base_DisplayForm<T> : Base_DisplayForm, ISetData<T>
+    {
+        //Implements ISetData<T>
+        abstract public void SetData(T t);
+    }
+    abstract public class Base_DisplayForm<T1, T2> : Base_DisplayForm, ISetData<T1, T2>
+    {
+        //Implements ISetData<T1, T2>
+        abstract public void SetData(T1 t1, T2 t2);
+    }
 
-    //public class Base_DisplayForm<T1, T2> : Base_DisplayForm, ISetData<T1, T2>
-    //{
-    //    public Base_DisplayForm()
-    //    {
-    //    }
-    //    //Implements ISetData<T1, T2>
-    //    virtual public void SetData(T1 t1, T2 t2) { }
-    //}
-
-    //public interface ISetData<T>
-    //{
-    //    void SetData(T t);
-    //}
-    //public interface ISetData<T1, T2>
-    //{
-    //    void SetData(T1 t1, T2 t2);
-    //}
+    public interface ISetData<T>
+    {
+        void SetData(T t);
+    }
+    public interface ISetData<T1, T2>
+    {
+        void SetData(T1 t1, T2 t2);
+    }
 }
--- CreatorCode/SolimarLicensing/common/LicAttribsCS/Lic_UsageInfoAttribs.cs
+++ CreatorCode/SolimarLicensing/common/LicAttribsCS/Lic_UsageInfoAttribs.cs
@@ -64,104 +64,10 @@ namespace Solimar
 						;
 					}
 					
-					[FlagsAttribute]
-					public enum TUsageFlag : uint
-					{
-						ufNone = 0,
-						ufUsePrimaryLic = 1,
-						ufUseFailoverLic = 2,
-					};
 					
-					public class AttribsMemberEnum_TUsageFlag : AttribsMemberEnum
-					{
-						public AttribsMemberEnum_TUsageFlag(string keyName, TUsageFlag defaultVal) :
-							base(keyName, defaultVal, typeof(TUsageFlag))
-						{
-							;
-						}
-						
-						protected static SortedList m_MapAliasToEnum;
-						protected static SortedList m_MapEnumToAlias;
-						protected static SortedList m_MapOrderingIndexToAlias;
-						protected static SortedList m_MapAliasToIndex;
-						protected static SortedList m_MapEnumToIndex;
-						static AttribsMemberEnum_TUsageFlag()
-						{
-							m_MapAliasToEnum = new SortedList();
-							m_MapEnumToAlias = new SortedList();
-							m_MapOrderingIndexToAlias = new SortedList();	// map of the ordering indexes from the xml file to aliases. The ordering indexes are not guranteed to be continuous or 0 based. 
-							m_MapAliasToIndex = new SortedList();	// This is different than the ordering index. This function takes an alias and returns what index it is in the GetAliases() list. 
-							m_MapEnumToIndex = new SortedList();	// This is different than the ordering index. This function takes an alias and returns what index it is in the GetAliases() list. 
-							m_MapAliasToEnum.Add("None",TUsageFlag.ufNone);
-							m_MapEnumToAlias.Add(TUsageFlag.ufNone,"None");
-							m_MapOrderingIndexToAlias.Add(1,"None");
-							m_MapAliasToEnum.Add("Using Primary Licensing",TUsageFlag.ufUsePrimaryLic);
-							m_MapEnumToAlias.Add(TUsageFlag.ufUsePrimaryLic,"Using Primary Licensing");
-							m_MapOrderingIndexToAlias.Add(2,"Using Primary Licensing");
-							m_MapAliasToEnum.Add("Using Failover Licensing",TUsageFlag.ufUseFailoverLic);
-							m_MapEnumToAlias.Add(TUsageFlag.ufUseFailoverLic,"Using Failover Licensing");
-							m_MapOrderingIndexToAlias.Add(3,"Using Failover Licensing");
-							m_MapAliasToIndex.Add("None",0);
-							m_MapAliasToIndex.Add("Using Primary Licensing",1);
-							m_MapAliasToIndex.Add("Using Failover Licensing",2);
-							m_MapEnumToIndex.Add(TUsageFlag.ufNone,0);
-							m_MapEnumToIndex.Add(TUsageFlag.ufUsePrimaryLic,1);
-							m_MapEnumToIndex.Add(TUsageFlag.ufUseFailoverLic,2);
-						}
-						
-					
-						public static string GetAlias(System.Enum enum_value)
-						{
-							return (string)m_MapEnumToAlias[enum_value];
-						}
-						public static StringCollection GetAliases()
-						{
-							StringCollection alias_list = new StringCollection();
-							foreach (string alias in m_MapOrderingIndexToAlias.Values)
-							{
-								alias_list.Add(alias);
-							}
-							return alias_list;
-						}
-						public static System.Enum GetEnumValueFromAlias(string alias)
-						{
-							return (System.Enum)m_MapAliasToEnum[alias];
-						}
-						public static int GetIndexFromAlias(string alias)
-						{
-							return (int)m_MapAliasToIndex[alias];
-						}
-						public static int GetIndexFromEnum(System.Enum enum_value)
-						{
-							return (int)m_MapEnumToIndex[enum_value];
-						}
-						
-						public string GetAlias()
-						{
-							return GetAlias(EVal);
-						}
-						public void SetEnumValueFromAlias(string alias)
-						{
-							EVal = GetEnumValueFromAlias(alias);
-						}
-					
-						public static implicit operator TUsageFlag(AttribsMemberEnum_TUsageFlag t)
-						{
-							return (TUsageFlag)t.m_tVal;
-						}
-						
-						public TUsageFlag TVal
-						{
-							get {return this;}
-							set {m_tVal = (Object)value;}
-						}
-					}
-					
-						
 					public class Lic_UsModuleInfoAttribsList : AttribsMemberGenericList {public Lic_UsModuleInfoAttribsList(string keyName, ArrayList defaultVal) : base(keyName, typeof(Lic_UsModuleInfoAttribs), defaultVal){;} }
 						
 					public AttribsMemberString applicationInstance = new AttribsMemberString("aI", "");
-					public AttribsMemberEnum_TUsageFlag usageFlag = new AttribsMemberEnum_TUsageFlag("uF", TUsageFlag.ufNone);
 					public Lic_UsModuleInfoAttribsList moduleList = new Lic_UsModuleInfoAttribsList("mLt", new ArrayList());
 				
 				};
