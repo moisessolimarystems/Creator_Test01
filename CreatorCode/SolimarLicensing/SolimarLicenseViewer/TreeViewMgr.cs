e3e9294580155471820c767842b41ceba4705c92
Alan Chou <alan.chou@solimarsystems.com>
2011-06-15 07:01:22 -0700

    added Library Services icon

--- CreatorCode/SolimarLicensing/SolimarLicenseViewer/TreeViewMgr.cs
+++ CreatorCode/SolimarLicensing/SolimarLicenseViewer/TreeViewMgr.cs
@@ -37,6 +37,7 @@ namespace SolimarLicenseViewer
             PRODUCTCONNECTIONSETTINGS,
             SOLITRACK,
             SOLSEARCHERENTERPRISESINGLEPLATFORM,
+            LIBRARYSERVICES,
             EVENTLOG,
         }
 
