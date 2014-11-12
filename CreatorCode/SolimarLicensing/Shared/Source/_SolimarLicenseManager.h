0ed6e80421189f6a2796e90af0661b0526c25e1b
Alan Chou <alan.chou@solimarsystems.com>
2014-11-12 02:07:48 -0800

    build_Creator3.0.264

--- CreatorCode/SolimarLicensing/Shared/Source/_SolimarLicenseManager.h
+++ CreatorCode/SolimarLicensing/Shared/Source/_SolimarLicenseManager.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Nov 11 22:12:15 2014
+/* at Wed Nov 12 10:03:30 2014
  */
 /* Compiler settings for _SolimarLicenseManager.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
