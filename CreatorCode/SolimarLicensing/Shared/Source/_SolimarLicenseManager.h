288191ce32c12a6fda0a844aad7c7c1294787cb5
mksuser <systems@solimarsystems.com>
2017-06-13 02:34:51 -0700

    build_Creator3.0.298

--- CreatorCode/SolimarLicensing/Shared/Source/_SolimarLicenseManager.h
+++ CreatorCode/SolimarLicensing/Shared/Source/_SolimarLicenseManager.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue May 30 17:03:55 2017
+/* at Tue Jun 13 09:31:12 2017
  */
 /* Compiler settings for _SolimarLicenseManager.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
