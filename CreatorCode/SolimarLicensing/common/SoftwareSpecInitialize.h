7199c727a9c97dc7cc190b60255897441f2127f6
mksuser <systems@solimarsystems.com>
2017-09-07 06:38:24 -0700

    Add EQUIOS JDF/JMF(Mod ID 5) to SOLitrack. Bumped software spec to 49.

--- CreatorCode/SolimarLicensing/common/SoftwareSpecInitialize.h
+++ CreatorCode/SolimarLicensing/common/SoftwareSpecInitialize.h
@@ -3,7 +3,7 @@
 
 softwareSpec.softwareSpec_SubMajor = 0;
 softwareSpec.softwareSpec_Major = 1;
-softwareSpec.softwareSpec_SubMinor = 48;
+softwareSpec.softwareSpec_SubMinor = 49;
 softwareSpec.softwareSpec_Minor = 0;
 InititalizeProduct115();
 InititalizeProduct114();
