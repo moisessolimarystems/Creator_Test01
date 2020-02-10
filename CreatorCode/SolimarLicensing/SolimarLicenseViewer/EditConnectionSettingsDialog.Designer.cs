c053a096be1cdd649456a926a5854a5bf0645302
Alan Chou <alan.chou@solimarsystems.com>
2020-02-10 08:38:19 -0800

    Remove unused code and general file cleanup build_Creator3.0.334

--- CreatorCode/SolimarLicensing/SolimarLicenseViewer/EditConnectionSettingsDialog.Designer.cs
+++ CreatorCode/SolimarLicensing/SolimarLicenseViewer/EditConnectionSettingsDialog.Designer.cs
@@ -42,17 +42,18 @@
             this.singleProdPropertyGrid.Location = new System.Drawing.Point(0, 0);
             this.singleProdPropertyGrid.Name = "singleProdPropertyGrid";
             this.singleProdPropertyGrid.PropertySort = System.Windows.Forms.PropertySort.NoSort;
-            this.singleProdPropertyGrid.Size = new System.Drawing.Size(396, 193);
+            this.singleProdPropertyGrid.Size = new System.Drawing.Size(396, 198);
             this.singleProdPropertyGrid.TabIndex = 3;
+            this.singleProdPropertyGrid.ToolbarVisible = false;
             this.singleProdPropertyGrid.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.singleProdPropertyGrid_PreviewKeyDown);
             // 
             // mainPanel
             // 
             this.mainPanel.Controls.Add(this.singleProdPropertyGrid);
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainPanel.Location = new System.Drawing.Point(0, 50);
+            this.mainPanel.Location = new System.Drawing.Point(0, 45);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(396, 193);
+            this.mainPanel.Size = new System.Drawing.Size(396, 198);
             this.mainPanel.TabIndex = 4;
             // 
             // headerPanel
@@ -62,20 +63,22 @@
             this.headerPanel.Location = new System.Drawing.Point(0, 0);
             this.headerPanel.Name = "headerPanel";
             this.headerPanel.Padding = new System.Windows.Forms.Padding(3);
-            this.headerPanel.Size = new System.Drawing.Size(396, 50);
+            this.headerPanel.Size = new System.Drawing.Size(396, 45);
             this.headerPanel.TabIndex = 5;
             // 
             // warningLabel
             // 
             this.warningLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.warningLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.warningLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.warningLabel.Location = new System.Drawing.Point(3, 3);
+            this.warningLabel.Margin = new System.Windows.Forms.Padding(3);
             this.warningLabel.Name = "warningLabel";
-            this.warningLabel.Size = new System.Drawing.Size(390, 44);
+            this.warningLabel.Size = new System.Drawing.Size(390, 39);
             this.warningLabel.TabIndex = 0;
-            this.warningLabel.Text = "Warning: Any License Setting change will not take effect until the product is res" +
-                "tarted. If the product is running as a service, the service will need to be rest" +
-                "arted.";
+            this.warningLabel.Text = "***Warning: Any License Setting change will not take effect until the product is " +
+    "restarted. If the product is running as a service, the service will need to be r" +
+    "estarted.";
             // 
             // EditConnectionSettingsDialog
             // 
