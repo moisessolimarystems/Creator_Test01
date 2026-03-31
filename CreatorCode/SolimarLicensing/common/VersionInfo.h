3a70c80e4a3420fb15bdd4f2febbcad7667b6b3b
mksuser <systems@solimarsystems.com>
2026-03-31 00:42:53 -0700

    Added member

---
+++ CreatorCode/SolimarLicensing/common/VersionInfo.h
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------------------*
+//    filename - VersionInfo.h                                             *
+//-------------------------------------------------------------------------*
+//-------------------------------------------------------------------------*
+//                                                                         *
+//    License Viewer		                                               *
+//                                                                         *
+//    Copyright (c) 1992-2001 by Solimar Systems, Inc.                     *
+//    All Rights Reserved.                                                 *
+//                                                                         *
+//-------------------------------------------------------------------------*
+#ifndef __VERSIONINFO_H
+#define __VERSIONINFO_H
+
+// Version of the program
+#define MAJOR_REVISION_NUMBER 1
+#define MINOR_REVISION_NUMBER 02
+
+//Build Number of the program
+#define BUILD_NUMBER 999999
+
+#endif
