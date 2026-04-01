93fa3b2018d89c83c339ee5a8c694a008a4d45b2
mksuser <systems@solimarsystems.com>
2026-04-01 05:20:31 -0700

    Added member

---
+++ CreatorCode/CreatorClient/PacketVerification.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Client.Creator
+{
+    partial class PacketVerification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.verificationCodeTextBox = new System.Windows.Forms.TextBox();
+            this.VerificationFileBrowseButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.57508F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.42492F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.verificationCodeTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.VerificationFileBrowseButton, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 53.42466F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(343, 48);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 7, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Verification Code:";
+            // 
+            // verificationCodeTextBox
+            // 
+            this.verificationCodeTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.verificationCodeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verificationCodeTextBox.Location = new System.Drawing.Point(129, 16);
+            this.verificationCodeTextBox.Margin = new System.Windows.Forms.Padding(3, 10, 0, 3);
+            this.verificationCodeTextBox.Name = "verificationCodeTextBox";
+            this.verificationCodeTextBox.Size = new System.Drawing.Size(182, 22);
+            this.verificationCodeTextBox.TabIndex = 1;
+            // 
+            // VerificationFileBrowseButton
+            // 
+            this.VerificationFileBrowseButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.VerificationFileBrowseButton.BackColor = System.Drawing.SystemColors.Control;
+            this.VerificationFileBrowseButton.Location = new System.Drawing.Point(311, 16);
+            this.VerificationFileBrowseButton.Margin = new System.Windows.Forms.Padding(0, 10, 3, 3);
+            this.VerificationFileBrowseButton.Name = "VerificationFileBrowseButton";
+            this.VerificationFileBrowseButton.Size = new System.Drawing.Size(25, 23);
+            this.VerificationFileBrowseButton.TabIndex = 2;
+            this.VerificationFileBrowseButton.Text = "...";
+            this.VerificationFileBrowseButton.UseVisualStyleBackColor = false;
+            this.VerificationFileBrowseButton.Click += new System.EventHandler(this.VerificationFileBrowseButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "vData";
+            this.openFileDialog.Filter = "License Verification Data|*.vData";
+            // 
+            // PacketVerification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(343, 79);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PacketVerification";
+            this.Text = "Packet Verification";
+            this.FinishDialog += new Shared.VisualComponents.FinishDialogEventHandler(this.PacketVerification_FinishDialog);
+            this.InitDialog += new Shared.VisualComponents.InitDialogEventHandler(this.PacketVerification_InitDialog);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PacketVerification_FormClosing);
+            this.Controls.SetChildIndex(this.tableLayoutPanel1, 0);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox verificationCodeTextBox;
+        private System.Windows.Forms.Button VerificationFileBrowseButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
---
+++ CreatorCode/CreatorClient/PacketVerification.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Client.Creator.CreatorService;
+using System.Security.Principal;
+using System.IO;
+using Solimar.Licensing.Attribs;
+
+namespace Client.Creator
+{
+    public partial class PacketVerification : Shared.VisualComponents.DialogBaseForm
+    {
+        private bool _validVerificationCode;
+        private string _selectedPacketName;
+        public PacketVerification()
+        {
+            _validVerificationCode = false;
+            InitializeComponent();
+        }
+
+        private void PacketVerification_InitDialog(object sender, Shared.VisualComponents.InitDialogEventArgs e)
+        {   
+            //_selectedPacketName = (e.Data as PacketVerificationDialogData).SelectedPacketName;
+        }
+
+        private void PacketVerification_FinishDialog(object sender, Shared.VisualComponents.FinishDialogEventArgs e)
+        {
+            PacketVerificationDialogData data = e.Data as PacketVerificationDialogData;
+            data.Verified = _validVerificationCode;
+            data.SelectedPacketName = _selectedPacketName;
+        }
+
+        private void PacketVerification_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DialogResult == DialogResult.OK)
+                if (!_validVerificationCode)
+                    e.Cancel = true;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            ValidateVerificationCode();
+        }
+
+        private void ValidateVerificationCode()
+        {
+            Client.Creator.ServiceProxy.Service<ICreator>.Use((client) =>
+            {
+                //_validVerificationCode = client.ValidateVerificationCode(_selectedPacketName, verificationCodeTextBox.Text);
+                //if (_validVerificationCode)
+                //{
+                    PacketTable storedPacket = client.GetPacketByVerificationCode(verificationCodeTextBox.Text);
+                    if (storedPacket != null)
+                    {
+                        storedPacket.IsVerified = true;
+                        storedPacket.VerifiedBy = WindowsIdentity.GetCurrent().Name;
+                        client.UpdatePacket(storedPacket);
+                        _selectedPacketName = storedPacket.PacketName;
+                        MessageBox.Show("Verified packet " + _selectedPacketName, 
+                                        "Successful Verification",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+                        _validVerificationCode = true;
+                    }
+                    else
+                    {
+                        // Set the ErrorProvider error with the text to display.
+                        MessageBox.Show("Verification code entered could not be validated for any packet.",
+                                        "Verification Code Error",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                        verificationCodeTextBox.Select(0, this.verificationCodeTextBox.Text.Length);      
+                    }
+                //}
+            });
+            //if (!_validVerificationCode)
+            //{                
+            //    // Set the ErrorProvider error with the text to display.
+            //    MessageBox.Show(string.Format("Verification code entered is invalid for packet : {0}", _selectedPacketName),
+            //                    "Verification Code Error",
+            //                    MessageBoxButtons.OK,
+            //                    MessageBoxIcon.Error);
+            //    verificationCodeTextBox.Select(0, this.verificationCodeTextBox.Text.Length);                    
+            //}
+         }
+
+        private void VerificationFileBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Byte[] licPktBytes = System.IO.File.ReadAllBytes(this.openFileDialog.FileName);
+                    string licStream = "";
+                    Client.Creator.ServiceProxy.Service<ICreator>.Use((client) =>
+                    {
+                        licStream = client.GenerateLicInfoByVerifyData(ref licPktBytes);
+                    });
+                    Lic_PackageAttribs licPackage = new Lic_PackageAttribs();
+                    licPackage.Stream = licStream;
+                    Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_VerificationCodeAttribs attrib = (Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_VerificationCodeAttribs)licPackage.licLicenseInfoAttribs.TVal.licVerificationAttribs.TVal.verificationCodeHistoryList.TVal[0];
+                    verificationCodeTextBox.Text = attrib.verificationValue;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+    }
+    #region PacketVerificationDialogData class
+    public class PacketVerificationDialogData : Shared.VisualComponents.DialogData
+    {
+        private string _packetName;
+        private bool _verified;
+
+        #region Constructors
+
+        public PacketVerificationDialogData()
+        {
+            _packetName = "";
+            _verified = false;
+        }
+        #endregion
+
+        #region Properties
+
+        public string SelectedPacketName
+        {
+            get { return this._packetName; }
+            set { this._packetName = value; }
+        }
+
+        public bool Verified
+        {
+            get { return _verified; }
+            set { _verified = true; }
+        }
+        #endregion
+    }
+    #endregion
+
+}
