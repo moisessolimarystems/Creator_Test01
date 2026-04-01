05ef6d359a33cd18a55baab8a24f1de04870b4f6
mksuser <systems@solimarsystems.com>
2026-04-01 07:28:42 -0700

    Added member

---
+++ CreatorCode/CreatorClient/ItemNameDialog.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Client.Creator
+{
+    partial class ItemNameDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ItemTypeLabel = new System.Windows.Forms.Label();
+            this.itemNameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(87, 4);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(6, 4);
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // ItemTypeLabel
+            // 
+            this.ItemTypeLabel.AutoSize = true;
+            this.ItemTypeLabel.Location = new System.Drawing.Point(13, 9);
+            this.ItemTypeLabel.Name = "ItemTypeLabel";
+            this.ItemTypeLabel.Size = new System.Drawing.Size(31, 13);
+            this.ItemTypeLabel.TabIndex = 5;
+            this.ItemTypeLabel.Text = "[Edit]";
+            // 
+            // itemNameTextBox
+            // 
+            this.itemNameTextBox.Location = new System.Drawing.Point(16, 29);
+            this.itemNameTextBox.Name = "itemNameTextBox";
+            this.itemNameTextBox.Size = new System.Drawing.Size(246, 20);
+            this.itemNameTextBox.TabIndex = 6;
+            // 
+            // ItemNameDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(273, 83);
+            this.Controls.Add(this.itemNameTextBox);
+            this.Controls.Add(this.ItemTypeLabel);
+            this.Name = "ItemNameDialog";
+            this.Text = "[Edit]";
+            this.FinishDialog += new Shared.VisualComponents.FinishDialogEventHandler(this.ItemNameDialog_FinishDialog);
+            this.Shown += new System.EventHandler(this.ItemNameDialog_Shown);
+            this.InitDialog += new Shared.VisualComponents.InitDialogEventHandler(this.ItemNameDialog_InitDialog);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ItemNameDialog_FormClosing);
+            this.Controls.SetChildIndex(this.ItemTypeLabel, 0);
+            this.Controls.SetChildIndex(this.itemNameTextBox, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ItemTypeLabel;
+        private System.Windows.Forms.TextBox itemNameTextBox;
+
+    }
+}
---
+++ CreatorCode/CreatorClient/ItemNameDialog.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Client.Creator.CreatorService;
+using Solimar.Licensing.Attribs;
+
+namespace Client.Creator
+{
+    public partial class ItemNameDialog : Shared.VisualComponents.DialogBaseForm
+    {
+        private string _name = "";
+        private bool _bValidItem = true;
+        private Object _selectedObject;        
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        #region ItemNameDialog Methods
+        public ItemNameDialog()
+        {
+            InitializeComponent();
+        }
+
+        public ItemNameDialog(string title)
+        {
+            InitializeComponent();
+            this.Text = title;
+            itemNameTextBox.Focus();
+        }
+
+        private void ValidateDialogInput(string inputString)
+        {
+            if (inputString != Name)
+            {
+                if (_selectedObject is CustomerDialogData)
+                    _bValidItem = (_selectedObject as CustomerDialogData).IsCustomerNameValid(inputString);
+                else
+                    _bValidItem = (_selectedObject as DestNameDialogData).IsDestNameValid(inputString);
+                if (!_bValidItem)
+                {
+                    MessageBox.Show(string.Format("Please enter a valid {0}.", _selectedObject.ToString()),
+                                    "Input Error",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    itemNameTextBox.Select(0, inputString.Length);
+                    itemNameTextBox.Focus();
+                }
+            }
+        }
+        #endregion
+
+        #region ItemNameDialog Events
+        private void ItemNameDialog_FinishDialog(object sender, Shared.VisualComponents.FinishDialogEventArgs e)
+        {
+            _name = itemNameTextBox.Text;
+        }
+
+        private void ItemNameDialog_InitDialog(object sender, Shared.VisualComponents.InitDialogEventArgs e)
+        {
+            if (e.Data is CustomerDialogData)
+            {
+                CustomerDialogData custData = e.Data as CustomerDialogData;
+                Name = itemNameTextBox.Text = custData.Customer;
+                ItemTypeLabel.Text = custData.ToString() + ":";
+            }
+            else
+            {
+                DestNameDialogData destNameData = e.Data as DestNameDialogData;
+                Name = itemNameTextBox.Text = destNameData.DestinationName;
+                ItemTypeLabel.Text = destNameData.ToString() + ":";
+            }
+            _selectedObject = e.Data;
+        }
+
+        private void ItemNameDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DialogResult == DialogResult.OK)
+                if (!_bValidItem)
+                    e.Cancel = true;
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            ValidateDialogInput(itemNameTextBox.Text);
+        }
+
+        private void ItemNameDialog_Shown(object sender, EventArgs e)
+        {
+            itemNameTextBox.Select(0, itemNameTextBox.Text.Length);
+            itemNameTextBox.Focus();
+        }
+        #endregion
+    }
+    #region CustomerDialogData class
+    public class CustomerDialogData : Shared.VisualComponents.DialogData
+    {
+        private string _customerName;
+
+        #region Constructors
+
+        public CustomerDialogData()
+        {
+            _customerName = "";
+        }
+        public CustomerDialogData(string custName)
+        {
+            _customerName = custName;
+        }
+        public bool IsCustomerNameValid(string custName)
+        {
+            bool bRetVal = false;
+            if(custName.Length > 0)
+            {
+                CustomerTable custRec = null;
+                Client.Creator.ServiceProxy.Service<ICreator>.Use((client) =>
+                {
+                    custRec = client.GetCustomer(custName, false);
+                });
+                if (custRec == null)
+                    bRetVal = true;
+            }
+            return bRetVal;
+        }        
+        public override string ToString()
+        {
+            return "Customer Name";
+        }
+        #endregion
+
+        #region Properties
+
+        public string Customer
+        {
+            get { return this._customerName; }
+            set { this._customerName = value; }
+        }
+        #endregion
+    }
+    #endregion
+
+    #region DestNameDialogData class
+    public class DestNameDialogData : Shared.VisualComponents.DialogData
+    {
+        private string _destName;
+        private uint _custID;
+
+        #region Constructors
+
+        public DestNameDialogData(uint custID)
+        {
+            _destName = "";
+            _custID = custID;
+        }
+        public DestNameDialogData(string destName, uint custID)
+        {
+            _destName = destName;
+            _custID = custID;
+        }
+        public bool IsDestNameValid(string destName)
+        {
+            bool bRetVal = false;
+            if (destName.Length > 0)
+            {
+                DestinationNameTable destNameRec = null;
+                Client.Creator.ServiceProxy.Service<ICreator>.Use((client) =>
+                {
+                    destNameRec = client.GetDestinationID((int)_custID, destName);
+                });
+                if (destNameRec == null)
+                    bRetVal = true;
+            }
+            return bRetVal;
+        }
+        public override string ToString()
+        {
+            return "Destination Name";
+        }
+        #endregion
+
+        #region Properties
+
+        public string DestinationName
+        {
+            get { return this._destName; }
+            set { this._destName = value; }
+        }
+        #endregion
+    }
+    #endregion
+}
