6df553cd256b21f0c17937e01cf996bf72daf4a0
mksuser <systems@solimarsystems.com>
2026-04-01 10:03:57 -0700

    Added member

---
+++ CreatorCode/CreatorClient/Entities/LicensePacket.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.Creator.CreatorService;
+using Solimar.Licensing.Attribs;
+using Client.Creator.ServiceProxy;
+
+namespace Client.Creator
+{
+    class LicensePacket
+    {
+        private Lic_PackageAttribs _licPackage;
+        private LicenseTable _licenseTable;
+        private string _licenseServer;
+        public LicensePacket(string licenseServer)        
+        {            
+            _licPackage = new Lic_PackageAttribs();
+            _licenseTable = null;
+            _licenseServer = licenseServer;
+        }
+
+        public Lic_PackageAttribs LicPackage
+        {
+            get { return _licPackage; }
+        }
+
+        public LicenseTable DatabaseRecord
+        {
+            get { return _licenseTable; }
+        }
+        
+        /// <summary>
+        /// add tokens
+        /// create products
+        /// add modules to products
+        /// </summary>
+        public bool BuildLicensePackage()
+        {
+            bool bRetVal = false;
+            Service<ICreator>.Use((client) =>
+            {
+                _licenseTable = client.GetLicenseByName(_licenseServer, false);
+            });
+            _licPackage.Stream = _licenseTable.LicenseInfo;
+            if (PopulateValidationTokens())
+            {
+                if (PopulateProductInfo())
+                {
+                    Lic_PackageAttribs.Lic_LicenseInfoAttribs licInfo = _licPackage.licLicenseInfoAttribs;
+                    Lic_LicenseInfoAttribsHelper.GenerateActivitySlotHistoryInfo(ref licInfo);
+                    _licPackage.licLicenseInfoAttribs.TVal = licInfo;
+                    _licenseTable.LicenseInfo = _licPackage.Stream;
+                    bRetVal = true;
+                }
+            }
+            return bRetVal;
+        }
+
+        public void SaveLicensePackage()
+        {
+            Lic_PackageAttribs.Lic_LicenseInfoAttribs licInfo = LicPackage.licLicenseInfoAttribs;
+            licInfo.licVerificationAttribs.TVal.validationTokenList.TVal.Clear();
+            licInfo.productList.TVal.Clear();
+            Service<ICreator>.Use((client) =>
+            {
+                _licenseTable.LicenseInfo = LicPackage.Stream;
+                client.UpdateLicense(_licenseTable, false);
+            });            
+        }
+
+        private bool PopulateValidationTokens()
+        {
+            bool bRetVal = true;
+            IList<SoftwareTokenTable> softwareTokens = null;
+            IList<TokenTable> tokenList = null;
+            Service<ICreator>.Use((client) =>
+            {               
+                softwareTokens = client.GetAllSoftwareTokens();
+                tokenList = client.GetTokensByLicenseName(_licenseServer);
+            });
+            _licPackage.licLicenseInfoAttribs.TVal.licVerificationAttribs.TVal.validationTokenList.TVal.Clear();
+            Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs licCodeToken = new Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs();
+            licCodeToken.tokenType.TVal = Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs.TTokenType.ttLicenseCode;
+            _licPackage.licLicenseInfoAttribs.TVal.licVerificationAttribs.TVal.validationTokenList.TVal.Add(licCodeToken);            
+            //add validation tokens for licinfo object
+            foreach (TokenTable token in tokenList)
+            {
+                if (tokenList.Count > 1)
+                {
+                    string tokenName = Enum.GetName(typeof(Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs.TTokenType), token.TokenType);
+                    SoftwareTokenTable st = softwareTokens.First(t => t.TokenType == tokenName);
+                    if (st.Status == 0)
+                        continue;
+                }
+                Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs newToken = new Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs();
+                newToken.tokenType.TVal = (Lic_PackageAttribs.Lic_LicenseInfoAttribs.Lic_ValidationTokenAttribs.TTokenType)token.TokenType;
+                newToken.tokenValue.TVal = token.TokenValue;
+                _licPackage.licLicenseInfoAttribs.TVal.licVerificationAttribs.TVal.validationTokenList.TVal.Add(newToken);
+            }
+            return bRetVal;
+        }
+
+        private bool PopulateProductInfo()
+        {
+            bool bRetVal = false;
+            Service<ICreator>.Use((client) =>
+            {
+                List<ProductLicenseTable> pltList = client.GetProductLicenses(_licenseServer);
+                foreach (ProductLicenseTable plt in pltList)
+                {
+                    Lic_PackageAttribs.Lic_ProductInfoAttribs product = new Lic_PackageAttribs.Lic_ProductInfoAttribs();
+                    product.contractNumber.TVal = plt.plID;
+                    product.productID.TVal = plt.ProductID;
+                    product.productState.TVal = (Lic_PackageAttribs.Lic_ProductInfoAttribs.TProductState)Enum.Parse(typeof(Lic_PackageAttribs.Lic_ProductInfoAttribs.TProductState), plt.plState.ToString());
+                    product.productAppInstance.TVal = plt.ProductConnection;
+                    string[] versionString = plt.ProductVersion.Split(".".ToCharArray());
+                    if(versionString.Count() > 0)
+                    {
+                        uint value;
+                        UInt32.TryParse(versionString[0], out value);
+                        product.product_Major.TVal = value;
+                        UInt32.TryParse(versionString[1], out value);
+                        product.product_Minor.TVal = value;
+                        product.product_SubMajor.TVal = 0;
+                        product.product_SubMinor.TVal = 0;
+                    }
+                    product.expirationDate.TVal = (plt.ExpirationDate.HasValue) ? plt.ExpirationDate.Value : new DateTime(1900, 1, 1);
+                    product.bUseExpirationDate.TVal = plt.ExpirationDate.HasValue;
+                    product.activationTotal.TVal = plt.Activations;
+                    product.activationAmountInDays.TVal = plt.ActivationAmount;
+                    product.bUseActivations.TVal = (plt.Activations > 0);
+                    List<TransactionTable> transactionList = client.GetNewTransactionsByLicenseName(_licenseServer);
+                    product.bActivationOverrideCurrent.TVal = (transactionList.Where(t => t.taOrderID == plt.ID &&
+                                                                                         (t.taType == (byte)TransactionType.ActivationAmount ||
+                                                                                          t.taType == (byte)TransactionType.ActivationTotal ||
+                                                                                          t.taType == (byte)TransactionType.ExpirationDate)).Count() > 0);
+                    List<ModuleTable> moduleList = client.GetModulesByProductLicense(plt.plID);
+                    if (moduleList.Count > 0)
+                    {
+                        foreach (ModuleTable module in moduleList)
+                        {
+                            Lic_PackageAttribs.Lic_ModuleInfoAttribs mod = new Lic_PackageAttribs.Lic_ModuleInfoAttribs();
+                            mod.moduleID.TVal = (uint)module.ModID;
+                            mod.moduleValue.TVal = (uint)module.Value;
+                            mod.moduleAppInstance.TVal = module.AppInstance;
+                            product.moduleList.TVal.Add(mod);
+                        }
+                    }
+                    _licPackage.licLicenseInfoAttribs.TVal.productList.TVal.Add(product);
+                }
+                bRetVal = true;
+            });
+            return bRetVal;
+        }
+    }
+}
---
+++ CreatorCode/CreatorClient/ServiceProxy/MyUIService.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using System.Globalization;
+
+namespace Client.Creator
+{
+    //Allows Propertygrid to display custom error messages instead of default error dialog.
+    internal class PropertyGridUIService : IUIService
+    {
+        private PropertyGrid propertyGrid;
+        private IDictionary styles;
+
+        internal PropertyGridUIService(PropertyGrid grid)
+        {
+            this.propertyGrid = grid;
+            this.styles = new Hashtable();
+        }
+
+        public bool CanShowComponentEditor(object component)
+        { // return value doesn't appear to matter
+            return false;
+        }
+
+        public IWin32Window GetDialogOwnerWindow()
+        {
+            return this.propertyGrid;
+        }
+
+        public void SetUIDirty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShowComponentEditor(object component, IWin32Window parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DialogResult ShowDialog(Form form)
+        {
+            if (form != null)
+            {
+                if (string.Compare(form.GetType().FullName, "System.Windows.Forms.PropertyGridInternal.GridErrorDlg", true, CultureInfo.InvariantCulture) == 0)
+                { // show custom error message dialog
+                    FieldInfo detailsTextBoxField = form.GetType().GetField("details", BindingFlags.NonPublic | BindingFlags.Instance);
+                    TextBox detailsTextBox = detailsTextBoxField.GetValue(form) as TextBox;
+
+                    return MessageBox.Show(this.propertyGrid,
+                        string.Format(CultureInfo.InvariantCulture, "{0}{1}{1}{2}", detailsTextBox.Text, Environment.NewLine, "Select Cancel to revert to the previously valid value."),
+                        this.propertyGrid.SelectedGridItem.PropertyDescriptor.DisplayName.Trim(),
+                        MessageBoxButtons.OKCancel,
+                        MessageBoxIcon.Warning);
+                }
+                else
+                { // show collection editor
+                    return form.ShowDialog(this.propertyGrid);
+                }
+            }
+
+            return DialogResult.Cancel;
+        }
+
+        public void ShowError(Exception ex, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DialogResult ShowMessage(string message, string caption, MessageBoxButtons buttons)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowMessage(string message, string caption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShowToolWindow(Guid toolWindow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary Styles
+        {
+            get
+            {
+                return this.styles;
+            }
+        }
+    }
+
+    internal class PropertyGridServiceProvider : IServiceProvider
+    {
+        private PropertyGridUIService service;
+
+        internal PropertyGridServiceProvider(PropertyGrid grid)
+        {
+            this.service = new PropertyGridUIService(grid);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(IUIService))
+            {
+                return this.service;
+            }
+            return null;
+        }
+    }
+}
