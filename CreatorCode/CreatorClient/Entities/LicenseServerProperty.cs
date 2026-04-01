ad409c251d6eddba302a1e7b5af559ae4d0495f4
mksuser <systems@solimarsystems.com>
2026-04-01 07:24:08 -0700

    Added member

---
+++ CreatorCode/CreatorClient/Entities/LicenseServerProperty.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using Solimar.Licensing.Attribs;
+using System.Reflection;
+using Client.Creator.CreatorService;
+using Client.Creator.ServiceProxy;
+
+namespace Client.Creator
+{
+    [TypeConverter(typeof(PropertySorterTypeConverter))]
+    public class LicenseServerProperty : BaseGridItem
+    {
+        #region Private Variables
+
+        private Lic_PackageAttribs.Lic_LicenseInfoAttribs _licInfo;
+        private string _destinationName;
+        private string _comments;
+        private PermissionsTable _permissions;
+
+        #endregion
+
+        public LicenseServerProperty()
+        {
+            _licInfo = new Lic_PackageAttribs.Lic_LicenseInfoAttribs();
+        }
+
+        public LicenseServerProperty(LicenseServerProperty license)
+        {
+            _licInfo = new Lic_PackageAttribs.Lic_LicenseInfoAttribs();
+            _licInfo.Stream = license.LicInfo.Stream;
+            _comments = license.Comments;
+            _permissions = license.Permissions;
+            _destinationName = license.DestName;
+        }
+
+        //requires a licPackage stream for licStream not just licLicenseInfoAttribs.
+        public LicenseServerProperty(LicenseTable license, PermissionsTable permissions)
+        {            
+            Lic_PackageAttribs licPackage = new Lic_PackageAttribs();
+            licPackage.Stream = license.LicenseInfo;            
+            _licInfo = new Lic_PackageAttribs.Lic_LicenseInfoAttribs();
+            _licInfo = licPackage.licLicenseInfoAttribs;           
+            _comments = license.LicenseComments;
+            Permissions = permissions;
+            Service<ICreator>.Use((client) => 
+            {
+                DestinationNameTable dest = client.GetDestinationName((int)CustID, (int)DestID);
+                if(dest != null)
+                    _destinationName = dest.DestName;
+            });
+        }
+
+        #region Properties
+        [Browsable(false)]
+        [System.ComponentModel.RefreshProperties(RefreshProperties.All)]
+        public PermissionsTable Permissions
+        {
+            get { return _permissions; }
+            set
+            {
+                _permissions = value;
+                PropertyDescriptor descriptor = TypeDescriptor.GetProperties(this.GetType())["ActivationCurrent"];
+                ReadOnlyAttribute attrib = (ReadOnlyAttribute)descriptor.Attributes[typeof(ReadOnlyAttribute)];
+                FieldInfo isReadOnly = attrib.GetType().GetField("isReadOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+                isReadOnly.SetValue(attrib, !_permissions.pt_create_modify_key.Value);
+                 descriptor = TypeDescriptor.GetProperties(this.GetType())["ActivationTotal"];
+                 attrib = (ReadOnlyAttribute)descriptor.Attributes[typeof(ReadOnlyAttribute)];
+                 isReadOnly = attrib.GetType().GetField("isReadOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+                isReadOnly.SetValue(attrib, !_permissions.pt_create_modify_key.Value);
+                descriptor = TypeDescriptor.GetProperties(this.GetType())["ActivationAmtInDays"];
+                attrib = (ReadOnlyAttribute)descriptor.Attributes[typeof(ReadOnlyAttribute)];
+                isReadOnly = attrib.GetType().GetField("isReadOnly", BindingFlags.NonPublic | BindingFlags.Instance);
+                isReadOnly.SetValue(attrib, !_permissions.pt_create_modify_key.Value);
+            }
+        }   
+
+        [Browsable(false)]
+        public Lic_PackageAttribs.Lic_LicenseInfoAttribs LicInfo
+        {
+            get { return _licInfo; }
+            set { _licInfo = value; }
+        }
+        
+        [Browsable(false)]
+        public bool HasUpdates
+        {
+            get
+            {
+                bool bValue = false;
+                Service<ICreator>.Use((client) =>
+                {
+                    bValue = client.IsLicenseUpdated(Name);
+                });
+                return !bValue;
+            }
+        }
+
+        [Browsable(false)]
+        public bool IsModified
+        {
+            get
+            {
+                bool bValue = false;
+                Service<ICreator>.Use((client) =>
+                {
+                    bValue = client.IsLicenseModified(Name);
+                });
+                return bValue;
+            }
+        }
+
+        [Browsable(false)]
+        public bool IsStandardLicenseType
+        {
+            get
+            {
+                if (LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltPerpetual ||
+                    LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltSubscription)
+                    return true;
+                return false;
+            }
+        }
+
+        [Browsable(false)]
+        public bool IsBackupLicenseType
+        {
+            get
+            {
+                if (LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltDisasterRecovery ||
+                    LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltFailover)
+                    return true;
+                return false;
+            }
+        }
+
+        [Browsable(false)]
+        public bool IsFailoverLicenseType
+        {
+            get
+            {
+                if (LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltFailover)
+                    return true;
+                return false;
+            }
+        }
+
+        [Browsable(false)]
+        public bool IsTestDevelopmentLicenseType
+        {
+            get
+            {
+                if (LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltTestDev)
+                    return true;
+                return false;
+            }
+        }
+
+        [Browsable(false)]
+        public bool IsEnabled
+        {
+            get
+            {
+                if (_licInfo.licVerificationAttribs.TVal.validationTokenList.TVal.Count > 1)
+                    return true;
+                return false;
+            }
+        }
+
+        [Browsable(false)]
+        public int FailoverLicenseCount
+        {
+            get
+            {
+                int count = 0;
+                Service<ICreator>.Use((client) =>
+                {
+                    count = client.GetLicenseCountByType(CustID, DestID, GroupID, Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltFailover);
+                });
+                return count;
+            }
+        }
+
+        [Category("License Server"), PropertyOrder(1)]
+        [DisplayName("Name")]
+        [Description("Name")] 
+        [ReadOnly(true)]
+        public override string Name
+        {
+            get
+            {
+                return Lic_LicenseInfoAttribsHelper.GetDisplayLabel(_licInfo);
+            }
+        }
+
+        [Browsable(false)]
+        public uint CustID
+        {
+            get { return _licInfo.customerID.TVal; }
+            set { _licInfo.customerID.TVal = value; }
+        }
+
+        [Browsable(false)]
+        public uint DestID
+        {
+            get { return _licInfo.destinationID.TVal; }
+            set { _licInfo.destinationID.TVal = value; }
+        }
+
+        [Browsable(false)]
+        public uint GroupID
+        {
+            get { return _licInfo.softwareGroupLicenseID.TVal; }
+            set { _licInfo.softwareGroupLicenseID.TVal = value; }
+        }
+
+        [Category("License Server"), PropertyOrder(2)]
+        [DisplayName("Destination Name")]
+        [Description("Destination Name")]
+        [ReadOnly(true)]
+        public string DestName
+        {
+            get { return _destinationName; }
+            set { _destinationName = value; }
+        }
+
+        [Category("License Server"), PropertyOrder(3)]
+        [DisplayName("Type")]
+        [Description("Type")]
+        [ReadOnly(true)]
+        public Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType LicType
+        {
+            get { return _licInfo.softwareLicType.TVal; }
+            set { _licInfo.softwareLicType.TVal = value; }
+        }
+
+        [Editor(typeof(System.ComponentModel.Design.MultilineStringEditor), typeof(System.Drawing.Design.UITypeEditor))]
+        [Category("License Server"), PropertyOrder(4)]
+        [DisplayName("Comments")]
+        [Description("Comments")]
+        public string Comments
+        {
+            get { return _comments; }
+            set { _comments = value; }
+        }
+
+        [Category("User Activations"), PropertyOrder(1)]
+        [DisplayName("Current")]
+        [Description("Current")]
+        [ReadOnly(true)]
+        public uint ActivationCurrent
+        {
+            get { return _licInfo.activationCurrent.TVal; }
+            set { _licInfo.activationCurrent.TVal = value; }
+        }
+
+
+        [Category("User Activations"), PropertyOrder(2)]
+        [DisplayName("Total")]
+        [Description("Total")]
+        [ReadOnly(true)]
+        public uint ActivationTotal
+        {
+            get { return _licInfo.activationTotal.TVal; }
+            set { _licInfo.activationTotal.TVal = value; }
+        }
+
+        [Category("User Activations"), PropertyOrder(3)]
+        [DisplayName("Amount in Days")]
+        [Description("Amount in Days")]
+        [ReadOnly(true)]
+        public uint ActivationAmtInDays
+        {
+            get { return _licInfo.activationAmountInDays.TVal; }
+            set { _licInfo.activationAmountInDays.TVal = value; }
+        }
+        #endregion
+
+    }
+}
---
+++ CreatorCode/CreatorClient/ProductLicenseSelectionForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Client.Creator.CreatorService;
+using Solimar.Licensing.Attribs;
+using Client.Creator.ServiceProxy;
+
+namespace Client.Creator
+{
+    public partial class ProductLicenseSelectionForm : Shared.VisualComponents.DialogBaseForm
+    {
+        private ListViewMgr _lvManager;
+
+        public ProductLicenseSelectionForm()
+        {
+            InitializeComponent();
+            _lvManager = new ListViewMgr();
+            _lvManager.SetListViewColumnSorter(listView1);
+        }
+
+        private void OrderSelectionForm_InitDialog(object sender, Shared.VisualComponents.InitDialogEventArgs e)
+        {            
+            LicenseServerProperty licData = (e.Data as OrderSelectionData).SelectedLicense;
+            Service<ICreator>.Use((client) =>
+            {
+                List<OrderTable> standardOrders = client.GetOrdersByLicenseName(string.Format("{0:x4}-{1:x3}-{2:x4}-P01", licData.CustID, licData.DestID, licData.GroupID));
+                List<OrderTable> subOrders = client.GetOrdersByLicenseName(licData.Name);
+                foreach (OrderTable order in standardOrders)
+                {
+                    if (order.OrderState != (byte)Lic_PackageAttribs.Lic_ModuleInfoAttribs.TModuleState.msAddOn)
+                    {
+                        //if order already exists apply check to checkbox and make greyed out? unselectable?
+                        ListViewItem lvItem = new ListViewItem();
+                        if (licData.LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltDisasterRecovery)
+                        {
+                            if (subOrders.Exists(c => c.OrderNumber.Equals(order.OrderNumber.Replace("P", "D"))))
+                                continue;
+                            //lvItem.Checked = true;
+                        }
+                        else if (licData.LicType == Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltTestDev)
+                        {
+                            if (subOrders.Exists(c => c.OrderNumber.Equals(order.OrderNumber.Replace("P", "T"))))
+                            {
+                                continue;
+                                //lvItem.Checked = true;
+                            }
+                        }
+                        lvItem.Text = order.OrderNumber;
+                        lvItem.SubItems.Add(Enum.GetName(typeof(Lic_PackageAttribs.Lic_ModuleInfoAttribs.TModuleState), order.OrderState));
+                        lvItem.SubItems.Add(order.ProductName);
+                        if (order.ExpirationDate.HasValue)
+                            lvItem.SubItems.Add(order.ExpirationDate.Value.ToShortDateString());
+                        else
+                            lvItem.SubItems.Add("");
+                        lvItem.SubItems.Add(order.Description);
+                        listView1.Items.Add(lvItem);
+                    }
+                }
+                _lvManager.AutoResizeColumns(listView1);
+            });
+        }
+
+        private void OrderSelectionForm_FinishDialog(object sender, Shared.VisualComponents.FinishDialogEventArgs e)
+        {
+            foreach (ListViewItem lvItem in listView1.Items)
+            {
+                if (lvItem.Checked)
+                    (e.Data as OrderSelectionData).OrderSelectionList.Add(lvItem.Text);
+            }
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            _lvManager.SetSortIndexColumn(listView1.Handle, e.Column);
+            // Perform the sort with these new sort options.
+            listView1.Sort();
+        }
+
+    }
+    #region OrderSelectionData class
+    public class OrderSelectionData : Shared.VisualComponents.DialogData
+    {
+        private LicenseServerProperty _selectedLicense;
+        private List<string> _orderSelectionList;
+        #region Constructors
+
+        public OrderSelectionData(LicenseServerProperty selectedLicense)
+        {
+            _selectedLicense = selectedLicense;
+            _orderSelectionList = new List<string>();
+        }
+        #endregion
+
+        #region Properties
+
+        public LicenseServerProperty SelectedLicense
+        {
+            get { return _selectedLicense; }
+            set { _selectedLicense = value; }
+        }
+
+        public List<string> OrderSelectionList
+        {
+            get { return _orderSelectionList; }
+            set { _orderSelectionList = value; }
+        }        
+        #endregion
+    }
+    #endregion
+}
