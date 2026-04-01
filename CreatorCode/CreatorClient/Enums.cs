f956615b0a70ee50e32a63c8399b3a86ae658e8c
mksuser <systems@solimarsystems.com>
2026-04-01 07:29:06 -0700

    Added member

---
+++ CreatorCode/CreatorClient/Enums.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Solimar.Licensing.Attribs;
+
+namespace Client.Creator
+{
+        #region Enums
+
+        enum ViewState
+        {
+            Customer,
+            LicenseServer
+        }
+             
+        enum TokenStatus
+        {
+            Reserved,
+            Active,
+            Deactivated
+        }
+
+        enum PacketListViewState
+        {
+            ALL,
+            VERIFIED,
+            UNVERIFIED
+        }
+
+        enum TransactionType
+        {
+            License,
+            Order,
+            Product
+        }
+
+        enum DetailTreeViewIconList
+        {
+            ICONVERT,
+            RUBIKA,
+            RUBIKAPROCESSBUILDER,
+            SDXDESIGNER,
+            SOLINDEXER,
+            SOLSEARCHERENTERPRISESINGLEPLATFORM,
+            SOLSEARCHERENTERPRISE,
+            SOLFUSION,
+            SOLSCRIPT,
+            SPDEQUEUEMANAGER,
+            SPDESYSTEMMANAGER,
+            SPDE,
+            SOLITRACK,
+            SPD,
+            CUSTOMER,
+            LICENSE,
+            LICENSEUPDATE,
+            LICENSEDISABLED,
+            LICENSERESYNC,
+            LICENSEDEACTIVATED,
+            ORDER,
+            ADDONORDER,
+            EMPTY
+        }
+
+        enum ListViewIconList
+        {
+            VERIFIED,
+            UNVERIFIED,
+            LISTBULLETS,
+            TRANSACTION_LOGFILE,
+            ZIPPEDFILE,
+            TTCOMPUTERNAME,
+            TTHARDWAREKEYID,
+            TTMACADDRESS,
+            TTBIOSSERIALNUMBER,
+            TTLICENSECODE,
+            MODULE,
+            VALIDATIONTOKENS
+        }
+
+        public enum LicenseServerType
+        {
+            Perpetual,
+            Failover,
+            DisasterRecovery,
+            TestDevelopment,
+            Subscription,
+            Deactivated
+        }
+
+        public enum ProductLicenseType
+        {
+            Trial,
+            Licensed,
+            AddOn,
+            Deactivated
+        }
+
+        public class Enums
+        {
+            public static int GetDetailTreeViewIconIndex(String key)
+            {
+                DetailTreeViewIconList iconIndex;
+                try
+                {
+                    iconIndex = (DetailTreeViewIconList)Enum.Parse(typeof(DetailTreeViewIconList), key.Replace(" ", ""), true);
+                }
+                catch (Exception ex)
+                {
+                    return -1;
+                }
+                return (int)iconIndex;
+            }
+
+            public static int GetListViewIconIndex(String key)
+            {
+                ListViewIconList iconIndex;
+                try
+                {
+                    iconIndex = (ListViewIconList)Enum.Parse(typeof(ListViewIconList), key.Replace(" ", ""), true);
+                }
+                catch (Exception ex)
+                {
+                    return -1;
+                }
+                return (int)iconIndex;
+            }
+
+            public static int GetLicenseServerType(LicenseServerType lsType)
+            {
+                int licenseType;
+                switch (lsType)
+                {
+                    case LicenseServerType.DisasterRecovery:
+                        licenseType = (int)Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltDisasterRecovery;
+                        break;
+                    case LicenseServerType.Failover:
+                        licenseType = (int)Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltFailover;
+                        break;
+                    case LicenseServerType.Perpetual:
+                        licenseType = (int)Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltPerpetual;
+                        break;
+                    case LicenseServerType.Subscription:
+                        licenseType = (int)Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltSubscription;
+                        break;
+                    case LicenseServerType.TestDevelopment:
+                        licenseType = (int)Lic_PackageAttribs.Lic_LicenseInfoAttribs.TSoftwareLicenseType.sltTestDev;
+                        break;
+                    default: licenseType = -1; break;
+                }
+                return licenseType;
+            }
+
+            public static int GetProductLicenseType(ProductLicenseType plType)
+            {
+                int productLicenseType;
+                switch (plType)
+                {
+                    case ProductLicenseType.Trial:
+                        productLicenseType = (int)Lic_PackageAttribs.Lic_ModuleInfoAttribs.TModuleState.msTrial;
+                        break;
+                    case ProductLicenseType.AddOn:
+                        productLicenseType = (int)Lic_PackageAttribs.Lic_ModuleInfoAttribs.TModuleState.msAddOn;
+                        break;
+                    case ProductLicenseType.Licensed:
+                        productLicenseType = (int)Lic_PackageAttribs.Lic_ModuleInfoAttribs.TModuleState.msLicensed;
+                        break;
+                    default: productLicenseType = -1; break;
+                }
+                return productLicenseType;
+            }
+
+            public static String GetLicenseServerPrefix(LicenseServerType licType)
+            {
+                String prefix = "";
+                if (licType != LicenseServerType.Deactivated)
+                    prefix = licType.ToString().Substring(0, 1);
+                return prefix;
+            }
+        }
+        #endregion
+}
---
+++ CreatorCode/CreatorData/ProductLicenseTable.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreatorData
+{
+    public partial class ProductLicenseTable
+    {
+        public static IList<ProductLicenseTable> GetAllProductLicenses()
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ObjectTrackingEnabled = false;
+                return db.ProductLicenseTables.ToList();
+            }
+        }
+
+        public static IList<ProductLicenseTable> GetProductLicenses(string licenseServerName)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ObjectTrackingEnabled = false;
+                return db.ProductLicenseTables.Where(c => c.LicenseID > 0 && c.LicenseTable.LicenseName.Equals(licenseServerName)).ToList();
+            }
+        }
+
+        public static ProductLicenseTable GetProductLicense(string productLicenseID)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ObjectTrackingEnabled = false;
+                return db.ProductLicenseTables.Where(c => c.LicenseID > 0 && c.plID.Equals(productLicenseID)).SingleOrDefault();
+            }
+        }
+
+        public static IList<ProductLicenseTable> GetProductLicenses(string licenseServerName, int productID)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {                
+                db.ObjectTrackingEnabled = false;
+                return db.ProductLicenseTables.Where(c => c.LicenseID > 0 && c.LicenseTable.LicenseName.Equals(licenseServerName) &&
+                                           c.ProductID.Equals(productID)).ToList();
+            }
+        }
+
+        public static IList<String> GetAddOnProductLicenses(string productLicenseID)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ObjectTrackingEnabled = false;
+                var addonOrderNames = from c in db.ProductLicenseTables
+                                      where c.LicenseID > 0
+                                      where c.ParentProductLicenseID.Equals(productLicenseID)
+                                      select c.plID;
+                return addonOrderNames.ToList();                
+            }
+        }
+
+        //how to get unique order index
+        //save last used index in license table
+        //update everytime order gets added 
+        public static int GetNextProductLicenseIndex(string licenseServerName)
+        {            
+            //int orderIndex = 0;
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ObjectTrackingEnabled = false;
+                LicenseTable license = db.LicenseTables.Where(c => c.LicenseName.Equals(licenseServerName)).FirstOrDefault();
+                if (license != null)                
+                    return license.OrderIndex;                
+                return 0;
+            }
+        }
+
+        public static int GetProductVersionFromTable(int productID)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ObjectTrackingEnabled = false;
+                var versions = from p in db.ProductTables
+                              where p.pId.Equals(productID)
+                              select p;
+                if (versions.Count() > 0)
+                    return versions.FirstOrDefault().pVersion;
+                return 0;
+            }
+        }
+
+        public static void DeleteAllProductLicenses(string licenseServer)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                IList<ProductLicenseTable> pltRecords = (from c in db.ProductLicenseTables
+                                 where c.plID.Contains(licenseServer)
+                                 select c).ToList();
+                db.ProductLicenseTables.DeleteAllOnSubmit(pltRecords);
+                db.SubmitChanges();
+            }
+        }
+
+        public static void CreateProductLicense(ProductLicenseTable contract)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ProductLicenseTables.InsertOnSubmit(contract);
+                db.SubmitChanges();
+            }
+        }
+
+        public static void UpdateProductLicense(ProductLicenseTable contract)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ProductLicenseTables.Attach(contract, true);
+                db.SubmitChanges();
+            }
+        }
+
+        public static void DeleteProductLicense(ProductLicenseTable contract)
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                db.ProductLicenseTables.Attach(contract);
+                db.ProductLicenseTables.DeleteOnSubmit(contract);
+                db.SubmitChanges();
+            }
+        }
+    }
+}
