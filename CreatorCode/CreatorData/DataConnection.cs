d1ca92fe55868e3bd86f684cdcd586b2effabdb7
mksuser <systems@solimarsystems.com>
2026-04-02 03:45:12 -0700

    Added member

---
+++ CreatorCode/CreatorData/DataConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace CreatorData
+{
+    public partial class CreatorDataContext
+    {
+        public CreatorDataContext()
+            : base(ConfigurationManager.ConnectionStrings["CreatorDB"].ConnectionString, mappingSource)
+        {
+        }
+    }
+}
