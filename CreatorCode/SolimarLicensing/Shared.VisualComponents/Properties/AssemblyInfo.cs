524d0d83977ba5cb96f590cab10f206c3e8e74c5
mksuser <systems@solimarsystems.com>
2026-04-02 04:48:28 -0700

    Added member

---
+++ CreatorCode/SolimarLicensing/Shared.VisualComponents/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Shared.VisualComponents.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Solimar.Licensing.AssemblyInfo.ASSEMBLY_COMPANY)]
+[assembly: AssemblyProduct(Solimar.Licensing.AssemblyInfo.ASSEMBLY_PRODUCTNAME)]
+[assembly: AssemblyCopyright(Solimar.Licensing.AssemblyInfo.ASSEMBLY_COPYRIGHT_NOTICE)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39439a27-96a7-4ef2-b908-58674a5aca53")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion(Solimar.Licensing.VersionInfo.PRODUCT_VERSION)]
+[assembly: AssemblyFileVersion(Solimar.Licensing.VersionInfo.PRODUCT_VERSION)]
