8e8fb6098d4fa211c24b8ea8f4e25ba5543dc2c4
mksuser <systems@solimarsystems.com>
2026-04-02 23:16:11 -0700

    Added member

---
+++ CreatorCode/SolimarLicensing/Shared.VisualComponents/BaseMessageDialog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shared.VisualComponents
+{
+    /// <summary>Represents base class for message dialogs</summary>
+    /// <example>
+    /// using (Shared.VisualComponents.BaseMessageDialog messageDialog = new Shared.VisualComponents.BaseMessageDialog())
+    /// {
+    ///		messageDialog.SetData(new Shared.VisualComponents.MessageBoxData());
+    ///		messageDialog.SetMessage("Looking for Server: \"{0}\"", serverName);
+    ///		messageDialog.Show(this);
+    ///		messageDialog.Update();
+    ///		...
+    ///		messageDialog.SetMessage("More stuff...");
+    ///		messageDialog.Update();
+    ///		...
+    ///		messageDialog.SetMessage("Still more stuff...");
+    ///		messageDialog.Update();
+    /// }
+    /// </example>
+    public partial class BaseMessageDialog : Form
+    {
+        public BaseMessageDialog()
+        {
+            InitializeComponent();
+            InternalInitialize();
+        }
+
+        private void InternalInitialize()
+        {
+        }
+
+        /// <remarks>Have not implemented cancel button</remarks>
+        public void SetData(MessageBoxData _data)
+        {
+            m_data = _data;
+            if (m_data != null)
+            {
+                cancelButton.Visible = m_data.bShowCancelButton;
+            }
+        }
+
+        /// <summary>Dispalys the formatted message to the message dialog.</summary>
+        /// <param name="_message">A composite format string.</param>
+        /// <param name="_args">Optional - An System.Object array containing zero or more objects to format.</param>
+        public void SetMessage(string _message, params object[] _args)
+        {
+            //Solimar.SSE.Logging.Log(Solimar.SSE.Logging.ELoggingFlags.Info, _message, _args);
+            messageLabel.Text = string.Format(_message, _args);
+        }
+
+        protected MessageBoxData m_data;
+    }
+    public class MessageBoxData
+    {
+        public bool bShowCancelButton = false;
+    }
+}
---
+++ CreatorCode/SolimarLicensing/SolimarLicenseDiagnosticDataViewer/Base_DisplayForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SolimarLicenseDiagnosticDataViewer
+{
+    abstract public class Base_DisplayForm : Form
+    {
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CP_NOCLOSE_BUTTON;	// Disable Close button
+                return cp;
+            }
+        }
+    }
+
+    abstract public class Base_DisplayForm<T> : Base_DisplayForm, ISetData<T>
+    {
+        //Implements ISetData<T>
+        abstract public void SetData(T t);
+    }
+    abstract public class Base_DisplayForm<T1, T2> : Base_DisplayForm, ISetData<T1, T2>
+    {
+        //Implements ISetData<T1, T2>
+        abstract public void SetData(T1 t1, T2 t2);
+    }
+
+    public interface ISetData<T>
+    {
+        void SetData(T t);
+    }
+    public interface ISetData<T1, T2>
+    {
+        void SetData(T1 t1, T2 t2);
+    }
+}
