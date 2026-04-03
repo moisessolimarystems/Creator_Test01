284c170a973d8a4bd5f984578644fc0abbb371f5
mksuser <systems@solimarsystems.com>
2026-04-03 07:10:47 -0700

    Added member

---
+++ CreatorCode/CreatorData/ProductLicenseTypeTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CreatorData
+{
+    public partial class ProductLicenseType
+    {
+        public static IList<ProductLicenseType> GetAllProductLicenseTypes()
+        {
+            using (CreatorDataContext db = new CreatorDataContext())
+            {
+                return db.ProductLicenseTypes.ToList();
+            }
+        }
+    }
+}
